9162308a4ea1d1473aae6cd9562c0c0d5139c517
ppinard <devnull@localhost>
2011-10-21 13:26:03 +0200

    - Add new base class for options converter

---
+++ pymontecarlo/input/base/convert.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+"""
+================================================================================
+:mod:`convert` -- Base class for conversion of options
+================================================================================
+
+.. module:: convert
+   :synopsis: Base class for conversion of options
+
+.. inheritance-diagram:: convert
+
+"""
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2011 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import copy
+import warnings
+
+# Third party modules.
+
+# Local modules.
+
+# Globals and constants variables.
+dc = copy.deepcopy
+
+class ConversionWarning(Warning):
+    pass
+
+class ConversionException(Exception):
+    pass
+
+class Converter(object):
+    def convert(self, old, newclass):
+        new = self._create_new_instance(old, newclass)
+
+        self._convert_beam(old, new)
+        self._convert_geometry(old, new)
+        self._convert_detectors(old, new)
+        self._convert_limits(old, new)
+
+        return new
+
+    def _create_new_instance(self, old, newclass):
+        return newclass(old.name)
+
+    def _convert_beam(self, old, new):
+        if old.beam.__class__ in new.BEAMS:
+            new.beam = dc(old.beam)
+        else:
+            raise ConversionException, "Cannot convert beam"
+
+    def _convert_geometry(self, old, new):
+        if old.geometry.__class__ in new.GEOMETRIES:
+            new.geometry = dc(old.geometry)
+        else:
+            raise ConversionException, "Cannot convert geometry"
+
+    def _convert_detectors(self, old, new):
+        for key, detector in old.detectors.iteritems():
+            if detector.__class__ in new.DETECTORS:
+                new.detectors[key] = dc(detector)
+            else:
+                message = "Detector '%s' of type '%s' was removed" % \
+                    (key, detector.__class__.__name__)
+                warnings.warn(message, ConversionWarning)
+
+    def _convert_limits(self, old, new):
+        for limit in old.limits:
+            if limit.__class__ in new.LIMITS:
+                new.limits.add(limit)
+            else:
+                message = "Limit of type '%s' was removed" % \
+                    (limit.__class__.__name__)
+                warnings.warn(message, ConversionWarning)
