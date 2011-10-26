468ba0928d28067ddfd6ac365cbce2a092df7459
ppinard <devnull@localhost>
2011-10-26 10:08:44 +0200

    - Set registration system in exporter. Derived class only need to register methods to export beam, geometry, detectors and limits parameters

--- pymontecarlo/input/base/exporter.py
+++ pymontecarlo/input/base/exporter.py
@@ -26,6 +26,65 @@ __license__ = "GPL v3"
 
 # Globals and constants variables.
 
+class ExporterWarning(Warning):
+    pass
+
+class ExporterException(Exception):
+    pass
+
 class Exporter(object):
+
+    def __init__(self):
+        self._beam_exporters = {}
+        self._geometry_exporters = {}
+        self._detector_exporters = {}
+        self._limit_exporters = {}
+
     def export(self, options):
-        raise NotImplementedError
+        self._export(options)
+
+    def _export(self, options, *args):
+        self._export_beam(options, *args)
+        self._export_geometry(options, *args)
+        self._export_detectors(options, *args)
+        self._export_limits(options, *args)
+
+    def _export_beam(self, options, *args):
+        clasz = options.beam.__class__
+        method = self._beam_exporters.get(clasz)
+
+        if method:
+            method(options, options.beam, *args)
+        else:
+            raise ExporterException, "Could not export beam '%s'" % clasz.__name__
+
+    def _export_geometry(self, options, *args):
+        clasz = options.geometry.__class__
+        method = self._geometry_exporters.get(clasz)
+
+        if method:
+            method(options, options.geometry, *args)
+        else:
+            raise ExporterException, "Could not export geometry '%s'" % clasz.__name__
+
+    def _export_detectors(self, options, *args):
+        for name, detector in options.detectors.iteritems():
+            clasz = detector.__class__
+            method = self._detector_exporters.get(clasz)
+
+            if method:
+                method(options, name, detector, *args)
+            else:
+                raise ExporterException, \
+                    "Could not export detector '%s' (%s)" % (name, clasz.__name__)
+
+    def _export_limits(self, options, *args):
+        for limit in options.limits:
+            clasz = limit.__class__
+            method = self._limit_exporters.get(clasz)
+
+            if method:
+                method(options, limit, *args)
+            else:
+                raise ExporterException, \
+                    "Could not export limit '%s'" % clasz.__name__
