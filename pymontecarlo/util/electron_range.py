a079316c6d5e9ab878c1292c240f0b1de95a1c33
ppinard <devnull@localhost>
2011-11-13 14:35:14 +0100

    New method to calculate the electron range based on the Kanaya-Okayama equation

---
+++ pymontecarlo/util/electron_range.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+"""
+================================================================================
+:mod:`electron_range` -- Electron range calculations
+================================================================================
+
+.. module:: electron_range
+   :synopsis: Electron range calculations
+
+.. inheritance-diagram:: pymontecarlo.util.electron_range
+
+"""
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2011 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+
+# Third party modules.
+
+# Local modules.
+import pymontecarlo.util.element_properties as ep
+
+# Globals and constants variables.
+
+def kanaya_okayama(composition, energy):
+    """
+    Returns the electron range (in meters).
+    
+    :arg composition: composition in weight fraction. 
+        The composition is specified by a dictionary.
+        The keys are the atomic numbers and the values are the weight fractions 
+        between ]0.0, 1.0]. 
+    :type composition: :class:`dict`
+    
+    :arg energy: beam energy in eV
+    """
+    r = 0.0;
+
+    for z, fraction in composition.iteritems():
+        dr = (0.0276 * ep.atomic_mass(z) * (energy / 1000.0) ** 1.67) / \
+            (z ** 0.89 * ep.mass_density(z))
+        r += fraction / (dr * 1e-6)
+
+    return 1.0 / r;
+
---
+++ pymontecarlo/util/test_electron_range.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+""" """
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2011 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import unittest
+import logging
+
+# Third party modules.
+
+# Local modules.
+from pymontecarlo.util.electron_range import kanaya_okayama
+
+# Globals and constants variables.
+
+class TestModule(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        self.comp1 = {29: 1.0}
+        self.comp2 = {29: 0.5, 30: 0.5}
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testkanaya_okayama(self):
+        self.assertAlmostEqual(1.45504, kanaya_okayama(self.comp1, 20e3) * 1e6, 4)
+        self.assertAlmostEqual(1.61829, kanaya_okayama(self.comp2, 20e3) * 1e6, 4)
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
