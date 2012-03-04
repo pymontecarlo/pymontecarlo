81f3fb54b21428981982aa6f707eda329926a8a3
Philippe T. Pinard <ppinard@L1600>
2012-03-04 13:54:43 +0100

    Add namespace to extractor classes in Java.

--- code/pymontecarlo/io/nistmonte/AbstractExtractor.java
+++ code/pymontecarlo/io/nistmonte/AbstractExtractor.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.jdom.Element;
+import org.jdom.Namespace;
 
 /**
  * Abstract implementation of the extractor interface.
@@ -12,6 +13,9 @@ import org.jdom.Element;
  * @author ppinard
  */
 public abstract class AbstractExtractor implements Extractor {
+
+    /** XML namespace. */
+    private final Namespace ns;
 
     /** XML tag. */
     private final Set<String> tags;
@@ -24,15 +28,33 @@ public abstract class AbstractExtractor implements Extractor {
      * @param tags
      *            required XML tag(s)
      */
-    public AbstractExtractor(String... tags) {
+    public AbstractExtractor(Namespace ns, String... tags) {
+        this.ns = ns;
         this.tags = new HashSet<String>();
         this.tags.addAll(Arrays.asList(tags));
     }
 
 
 
+    /**
+     * Creates a new <code>AbstractExtractor</code>.
+     * 
+     * @param tags
+     *            required XML tag(s)
+     */
+    public AbstractExtractor(String... tags) {
+        this(Namespace.getNamespace("mc",
+                "http://pymontecarlo.sf.net/input/base"), tags);
+    }
+
+
+
     @Override
     public boolean canExtract(Element element) {
-        return tags.contains(element.getName());
+        if (element.getNamespace().equals(Namespace.NO_NAMESPACE))
+            return tags.contains(element.getName());
+        else
+            return element.getNamespace().equals(ns)
+                    && tags.contains(element.getName());
     }
 }
