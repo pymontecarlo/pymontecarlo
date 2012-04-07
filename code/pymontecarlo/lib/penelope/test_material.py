8dbff8c7e5cce4ea36861ea4126de709cb7aa0e8
Philippe T. Pinard <ppinard@L1600>
2012-04-07 14:05:31 +0200

    Add builder for penelope lib in Eclipse. Add unit test for material.

---
+++ code/pymontecarlo/lib/penelope/test_material.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+""" """
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2012 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import unittest
+import logging
+import tempfile
+import os
+import shutil
+
+# Third party modules.
+
+# Local modules.
+import pymontecarlo.lib.penelope.material as material #@UnresolvedImport
+from pymontecarlo.input.base.material import pure
+from pymontecarlo import settings
+
+# Globals and constants variables.
+
+class TestMaterial(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        self.tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+        shutil.rmtree(self.tempdir, ignore_errors=True)
+
+    def testskeleton(self):
+        self.assertTrue(True)
+
+    def testcreate(self):
+        oldcwd = os.getcwd()
+        os.chdir(settings.penelope.pendbase)
+
+        filepath = os.path.join(self.tempdir, 'cu.mat')
+        material.create(pure(29), filepath)
+
+        with open(filepath, 'r') as fp:
+            lines = fp.readlines()
+            self.assertTrue(lines[0].startswith(' PENELOPE'))
+            self.assertTrue(lines[-1].startswith(' PENELOPE'))
+
+        os.chdir(oldcwd)
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
