6870a8f17c3f81473c61d846b3447d7d4b803e26
ppinard <devnull@localhost>
2012-04-27 17:13:06 +0200

    Add documentation.

--- code/pymontecarlo/util/config.py
+++ code/pymontecarlo/util/config.py
@@ -1,13 +1,86 @@
 #!/usr/bin/env python
 """
 ================================================================================
-:mod:`config` -- Read configuration file
+:mod:`config` -- Configuration parser
 ================================================================================
 
 .. module:: config
    :synopsis: Read configuration file
 
 .. inheritance-diagram:: pymontecarlo.util.config
+
+Configuration parser to read INI type files. 
+This parser differs slightly from those available in the :mod:`ConfigParser`
+module as the sections, options and values can be accessed using attributes
+instead of getters and setters.
+While the access of the data is different, this class uses the 
+:class:`SafeConfigParser` of the :mod:`ConfigParser` module to read and
+write configuration file.
+
+Let's take the configuration file::
+
+  [section1]
+  option1=value1
+  option2=value2
+  
+  [section2]
+  option3 = value3
+  
+To load the configuration file, use the :meth:`read` method::
+
+  >>> config = ConfigParser()
+  >>> with open(filepath, 'r') as fp:
+  ...     config.read(fp)
+
+To get the value of ``option1`` in ``section1``::
+
+  >>> config.section1.option1
+  'value1'
+
+or of ``option3`` in ``section2``::
+
+  >>> config.section2.option3
+  'value3'
+
+To check if a section is present, use the ``in`` statement::
+
+  >>> 'section3' in config
+  False
+  >>> 'option2' in config.section1
+  True
+
+To iterate over all sections and options::
+
+  >>> for section, option, value in config:
+  ...     print section, option, value
+  'section1' 'option1' 'value1'
+  'section1' 'option2' 'value2'
+  'section2' 'option3' 'value3'
+
+Note that the order may changed.
+
+To add a section or option::
+
+  >>> config.add_section('section3')
+  >>> 'section3' in config
+  True
+  >>> config.section3.option4 = 'value4'
+
+The following syntax can be shorten as :meth:`add_section` method returns the
+added section::
+
+  >>> config.add_section('section3').option4 = 'value4'
+
+If the added section already exists, no section is added and the current one
+is returned::
+
+  >>> config.add_section('section1').option1
+  'value1'
+
+Finally, the configuration can be saved back to a file::
+
+  >>> with open(filepath, 'w') as fp:
+  ...     config.write(fp)
 
 """
 
@@ -39,6 +112,9 @@ class _Section(object):
         return option_name in self.__dict__
 
 class ConfigParser(object):
+    """
+    Configuration parser.
+    """
 
     def __contains__(self, section_name):
         return section_name in self.__dict__
@@ -49,11 +125,22 @@ class ConfigParser(object):
                 yield section_name, option_name, value
 
     def add_section(self, section_name):
+        """
+        Adds a new section if the specified section name doesn't exist.
+        Does nothing if the section already exists.
+        
+        :return: section
+        """
         if section_name not in self.__dict__:
             self.__dict__[section_name] = _Section()
         return self.__dict__[section_name]
 
     def read(self, fileobj):
+        """
+        Reads the configuration from the file object.
+        
+        :arg fileobj: file object
+        """
         parser = SafeConfigParser()
         parser.readfp(fileobj)
 
@@ -66,6 +153,11 @@ class ConfigParser(object):
             self.__dict__[section] = _Section(options)
 
     def write(self, fileobj):
+        """
+        Writes the configuration inside the file object.
+        
+        :arg fileobj: file object
+        """
         parser = SafeConfigParser()
 
         # Add sections
