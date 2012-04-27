53a6bb60aff6b57be398f3c0fb0beb2dddef23e5
ppinard <devnull@localhost>
2012-04-27 14:18:11 +0200

    Create separate method for loading settings and programs. Add documentation.

--- code/pymontecarlo/__init__.py
+++ code/pymontecarlo/__init__.py
@@ -45,28 +45,49 @@ XMLIO.add_namespace('mc', 'http://pymontecarlo.sf.net',
 _settings = None
 
 def get_settings():
+    """
+    Returns the global settings.
+    
+    .. note:: 
+       
+       Note that the settings are loaded only once when this method is called
+       for the first time. After that, the same settings are returned.
+    
+    :return: settings
+    :rtype: :class:`pymontecarlo.util.config.ConfigParser`
+    """
     global _settings
 
     if _settings is not None:
         return _settings
-
-    _settings = ConfigParser()
 
     filepaths = []
     filepaths.append(os.path.join(os.path.expanduser('~'), '.pymontecarlo',
                                   'settings.cfg'))
     filepaths.append(os.path.join(os.path.dirname(__file__), 'settings.cfg'))
 
+    _settings = load_settings(filepaths)
+
+    return _settings
+
+def load_settings(filepaths):
+    """
+    Loads the settings from the first file path that exists from those 
+    specified.
+    
+    :arg filepaths: :class:`list` of paths to potential settings file
+    """
+    settings = ConfigParser()
+
     for filepath in filepaths:
         if os.path.exists(filepath):
             logging.debug('Loading settings from: %s', filepath)
 
             with open(filepath, 'r') as f:
-                _settings.read(f)
+                settings.read(f)
+                return settings
 
-            break
-
-    return _settings
+    raise IOError, "Settings could not be loaded"
 
 ################################################################################
 # Programs
@@ -75,18 +96,45 @@ def get_settings():
 _programs = None
 
 def get_programs():
+    """
+    Returns the available Monte Carlo programs.
+    The programs are loaded based on the settings.
+    
+    .. note:: 
+       
+       Note that the programs are loaded only once when this method is called
+       for the first time. After that, the same set is returned.
+    
+    :return: a set of programs
+    """
     global _programs
 
     if _programs is not None:
         return _programs
 
-    settings = get_settings()
+    _programs = frozenset(load_programs(get_settings()))
 
-    value = getattr(settings.pymontecarlo, 'extensions', '')
+    return _programs
+
+def load_programs(settings):
+    """
+    Loads the programs defined in the settings.
+    The list of programs should be set under the section ``pymontecarlo`` and
+    the option ``program``. An :exc:`IOError` exception is raised if the option
+    is not defined.
+    
+    Before a program is added to the list, the method makes sure that the
+    program is valid. 
+    
+    :arg settings: settings
+    """
+    programs = set()
+
+    value = getattr(settings.pymontecarlo, 'programs', '')
     if not value:
-        return
+        raise IOError, "No programs are defined in settings"
 
-    extensions = getattr(settings.pymontecarlo, 'extensions').split(',')
+    extensions = getattr(settings.pymontecarlo, 'programs').split(',')
     for extension in extensions:
         mod = __import__(extension, None, None, ['config'])
 
@@ -99,9 +147,20 @@ def get_programs():
         program = mod.config.program
         program.validate()
 
-        _programs.add(program)
+        programs.add(program)
         logging.debug("Loaded program (%s) from %s", program.name, extension)
 
+    return programs
+
+################################################################################
+# Reload
+################################################################################
+
 def reload():
+    """
+    Resets the settings and programs.
+    Once the methods :meth:`get_settings` or :meth:`get_programs` are called,
+    the settings and programs will be reloaded.
+    """
     _settings = None
     _programs = None
