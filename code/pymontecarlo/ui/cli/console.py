de50c965bb2ccf0603ce194b74b37fcc3f18a9e6
ppinard <devnull@localhost>
2012-04-29 17:52:27 +0200

    Add method to retrieve input from stdin.

--- code/pymontecarlo/ui/cli/console.py
+++ code/pymontecarlo/ui/cli/console.py
@@ -19,6 +19,7 @@ __copyright__ = "Copyright (c) 2012 Philippe T. Pinard"
 __license__ = "GPL v3"
 
 # Standard library modules.
+import os
 import sys
 import platform
 import textwrap
@@ -35,6 +36,7 @@ COLOR_RED = 'red'
 COLOR_GREEN = 'green'
 COLOR_BLUE = 'blue'
 COLOR_YELLOW = 'yellow'
+COLOR_PURPLE = 'purple'
 
 class ProgressBar(object):
     _BAR_TEMPLATE = 'Completed %i/%i | [%s%s] %i/100%% - %s'
@@ -48,7 +50,7 @@ class ProgressBar(object):
         self._width = width
 
     def start(self):
-        self._console.message('=' * self._console.width)
+        self._console.print_message('=' * self._console.width)
         self.update(0, 0, 'Start')
 
     def _create_bar_text(self, counter, progress, status):
@@ -77,7 +79,7 @@ class ProgressBar(object):
         self._console._post_print(stream, text, COLOR_GREEN)
         stream.flush()
 
-        self._console.message('=' * self._console.width)
+        self._console.print_message('=' * self._console.width)
 
 class ConsoleLoggingHandler(logging.Handler):
     def __init__(self, console, level=logging.NOTSET):
@@ -91,13 +93,13 @@ class ConsoleLoggingHandler(logging.Handler):
             levelno = record.levelno
 
             if levelno >= logging.ERROR:
-                self._console.error(msg)
+                self._console.print_error(msg)
             elif levelno >= logging.WARNING:
-                self._console.warn(msg)
+                self._console.print_warn(msg)
             elif levelno >= logging.INFO:
-                self._console.info(msg)
+                self._console.print_info(msg)
             elif levelno >= logging.DEBUG:
-                self._console.debug(msg)
+                self._console.print_debug(msg)
         except (KeyboardInterrupt, SystemExit):
             raise
         except:
@@ -105,6 +107,7 @@ class ConsoleLoggingHandler(logging.Handler):
 
 class _Console(object):
     _COLORS = {}
+    _PROMPT_PREFIX = "> "
 
     def __init__(self, width=80, stdout=sys.stdout, stderr=sys.stderr):
         self._width = width
@@ -132,41 +135,158 @@ class _Console(object):
     def _pre_print(self, stream, text, color):
         pass
 
-    def _print(self, stream, text, color=COLOR_DEFAULT):
+    def _print(self, stream, text, color=COLOR_DEFAULT,
+               eol=True, wrap=True, fill=True):
         self._pre_print(stream, text, color)
 
+        # Wrap
         lines = []
-        for line in textwrap.wrap(text, self.width, subsequent_indent='  '):
-            lines.append(line.ljust(self.width - 1))
+        if wrap:
+            for line in textwrap.wrap(text, self.width, subsequent_indent='  '):
+                lines.append(line)
+
+            if text.endswith((' ', '\t')): lines[-1] += ' '
+        else:
+            lines.append(text)
+
+        # Fill with white space
+        if fill:
+            for i in range(len(lines)):
+                lines[i] = lines[i].ljust(self.width - 1)
+
+        # Write
         stream.write('\n'.join(lines))
 
         self._post_print(stream, text, color)
-        stream.write('\n')
+        if eol: stream.write('\n')
         stream.flush()
 
     def _post_print(self, stream, text, color):
         pass
 
-    def error(self, text):
+    def print_error(self, text, exit=True):
         self._print(self._stderr, 'Error: ' + text, color=COLOR_RED)
-        sys.exit(1)
+        if exit: self.close(1)
 
-    def warn(self, text):
+    def print_warn(self, text):
         self._print(self._stderr, 'Warning: ' + text, color=COLOR_YELLOW)
 
-    def info(self, text):
+    def print_info(self, text):
         self._print(self._stderr, 'Info: ' + text, color=COLOR_DEFAULT)
 
-    def debug(self, text):
+    def print_debug(self, text):
         self._print(self._stderr, 'Debug: ' + text, color=COLOR_BLUE)
 
-    def message(self, text):
+    def print_message(self, text):
         self._print(self._stdout, text, color=COLOR_DEFAULT)
 
-    def success(self, text):
+    def print_success(self, text):
         self._print(self._stdout, text, color=COLOR_GREEN)
 
-    def close(self):
+    def print_line(self):
+        self._print(self._stdout, "-" * self._width)
+
+    def _prompt(self, question, default=None, validators=[]):
+        text = self._PROMPT_PREFIX + question
+        if default: text += ' [%s]' % default
+        text += ": "
+
+        while True:
+            self._print(self._stdout, text, COLOR_PURPLE, eol=False, fill=False)
+            answer = raw_input() or default
+
+            if answer is None: answer = ''
+            answer = answer.strip()
+
+            try:
+                for validator in reversed(validators):
+                    validator(answer)
+            except Exception as ex:
+                self.print_error(str(ex), False)
+                continue
+
+            break
+
+        return answer
+
+    def prompt_text(self, question, default=None, validators=[]):
+        # Validators
+        def _nonempty(answer):
+            if not answer:
+                raise ValueError, 'Please enter some text.'
+        validators.append(_nonempty)
+
+        # Prompt
+        return self._prompt(question, default, validators)
+
+    def prompt_boolean(self, question, default=None, validators=[]):
+        # Validators
+        def _boolean(answer):
+            if answer.upper() not in ('Y', 'YES', 'N', 'NO'):
+                raise ValueError, "Please enter either 'y' or 'n'"
+        validators.append(_boolean)
+
+        # Default
+        if default is not None:
+            default = 'y' if default else 'n'
+
+        # Prompt
+        answer = self.prompt_text(question, default, validators)
+        return answer.upper() in ('Y', 'YES')
+
+    def prompt_file(self, question, default=None, should_exist=True, validators=[]):
+        # Validators
+        def _exists(answer):
+            if should_exist and not os.path.isfile(answer):
+                raise ValueError, "Please enter an existing file"
+        validators.append(_exists)
+
+        # Prompt
+        answer = self.prompt_text(question, default, validators)
+
+        return os.path.abspath(answer)
+
+    def prompt_directory(self, question, default=None, should_exist=True, validators=[]):
+        # Validators
+        def _exists(answer):
+            if should_exist and not os.path.isdir(answer):
+                raise ValueError, "Please enter an existing file"
+        validators.append(_exists)
+
+        # Prompt
+        answer = self.prompt_text(question, default, validators)
+
+        return os.path.abspath(answer)
+
+    def prompt_float(self, question, default=None, validators=[]):
+        # Validators
+        def _float(answer):
+            try:
+                answer = float(answer)
+            except ValueError:
+                raise ValueError, "Cannot convert %s to float" % answer
+        validators.append(_float)
+
+        if default is not None: default = str(default)
+
+        # Prompt
+        return float(self.prompt_text(question, default, validators))
+
+    def prompt_int(self, question, default=None, validators=[]):
+        # Validators
+        def _int(answer):
+            try:
+                answer = int(answer)
+            except ValueError:
+                raise ValueError, "Cannot convert %s to integer" % answer
+        validators.append(_int)
+
+        if default is not None: default = str(default)
+
+        # Prompt
+        return int(self.prompt_text(question, default, validators))
+
+    def close(self, retcode=0):
         # Reset warnings
         warnings.showwarning = warnings._show_warning
 
@@ -175,11 +295,11 @@ class _Console(object):
         logger.handlers = []
         logging.basicConfig()
 
-        sys.exit(0)
+        sys.exit(retcode)
 
 class UnixConsole(_Console):
     _COLORS = {COLOR_DEFAULT: 0, COLOR_RED: 31, COLOR_GREEN: 32,
-               COLOR_BLUE: 34, COLOR_YELLOW: 33}
+               COLOR_BLUE: 34, COLOR_YELLOW: 33, COLOR_PURPLE: 35}
 
     def _pre_print(self, stream, text, color):
         stream.write("\033[%dm\033[1m" % self._COLORS[color])
@@ -192,7 +312,8 @@ if platform.system() == 'Windows':
 
     class WindowsConsole(_Console):
         _COLORS = {COLOR_DEFAULT: 15, COLOR_RED: 12, COLOR_GREEN: 10,
-                   COLOR_BLUE: 9, COLOR_YELLOW: 14}
+                   COLOR_BLUE: 9, COLOR_YELLOW: 14, COLOR_PURPLE: 15}
+        #FIXME: Find purple color in Windows
 
         def __init__(self, width=80, stdout=sys.stdout, stderr=sys.stderr):
             _Console.__init__(self, width, stdout, stderr)
