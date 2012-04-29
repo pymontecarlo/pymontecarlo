24156f8d0ba88ea9756af7dc69a23330f9a9dce3
ppinard <devnull@localhost>
2012-04-29 18:27:27 +0200

    Add documentation.

--- code/pymontecarlo/ui/cli/console.py
+++ code/pymontecarlo/ui/cli/console.py
@@ -9,6 +9,13 @@
 
 .. inheritance-diagram:: pymontecarlo.ui.cli.console
 
+Generic interface to interact with the command line.
+A console implements easy functions to print error, warning, information 
+messages as well as to retrieve data from the user.
+
+To ensure that the right console is used for the operating system, the
+method :meth:`create_console` should be called instead of the actual console
+constructor.
 """
 
 # Script information for the file.
@@ -39,9 +46,27 @@ COLOR_YELLOW = 'yellow'
 COLOR_PURPLE = 'purple'
 
 class ProgressBar(object):
-    _BAR_TEMPLATE = 'Completed %i/%i | [%s%s] %i/100%% - %s'
+    _BAR_TEMPLATE = 'Completed %i/%i | [%s%s] %i%% - %s'
 
     def __init__(self, console, total, fill_char='#', empty_char=' ', width=25):
+        """
+        Creates a progress bar to show the progress of task(s) in the command
+        line.
+        The progress bar displays two progresses: the number of tasks completed
+        and the progress of each task.
+        There is an example of the progress bar output::
+        
+          Completed 1/5 | [###########             ] 45%% - Running task 1
+        
+        Once created and initialized, the method :meth:`update` should be call
+        to redraws the progress bar in the command line.
+        
+        :arg console: console where the progress bar will be written
+        :arg total: total number of tasks
+        :arg fill_char: character used in the bar of the progress bar
+        :arg empty_char: character used to fill the progress bar
+        :arg width: width the progress bar
+        """
         self._console = console
         self._total = total
 
@@ -50,10 +75,21 @@ class ProgressBar(object):
         self._width = width
 
     def start(self):
+        """
+        Initializes this progress bar.
+        This method must be called before calling :meth:`update`
+        """
         self._console.print_message('=' * self._console.width)
         self.update(0, 0, 'Start')
 
     def _create_bar_text(self, counter, progress, status):
+        """
+        Constructs the text for this progress bar.
+        
+        :arg counter: number of completed tasks
+        :arg progress: progress of the running task (0.0 to 1.0)
+        :arg status: status of the running task
+        """
         x = int(progress * self._width)
         fill = self._fill_char * x
         empty = self._empty_char * (self._width - x)
@@ -65,12 +101,23 @@ class ProgressBar(object):
         return text
 
     def update(self, counter, progress, status):
+        """
+        Updates the progress and status of this progress bar.
+        
+        :arg counter: number of completed tasks
+        :arg progress: progress of the running task (0.0 to 1.0)
+        :arg status: status of the running task
+        """
         text = self._create_bar_text(counter, progress, status)
         stream = self._console._stdout
         stream.write(text + "\r")
         stream.flush()
 
     def close(self):
+        """
+        Closes this progress bar.
+        The progress bar will appear in green with *Done* as the status message.
+        """
         text = self._create_bar_text(self._total, 1.0, 'Done')
 
         stream = self._console._stdout
@@ -82,6 +129,10 @@ class ProgressBar(object):
         self._console.print_message('=' * self._console.width)
 
 class ConsoleLoggingHandler(logging.Handler):
+    """
+    Special logging handler to redirect log messages to the console.
+    """
+
     def __init__(self, console, level=logging.NOTSET):
         logging.Handler.__init__(self, level)
 
@@ -110,15 +161,34 @@ class _Console(object):
     _PROMPT_PREFIX = "> "
 
     def __init__(self, width=80, stdout=sys.stdout, stderr=sys.stderr):
+        """
+        Creates a new console.
+        The method :meth:`init` should be called to initialize the console
+        after it is created.
+        
+        :arg width: number of characters in one line [default: 80]
+            Text exceeding this number of characters will be wrapped to multiple
+            lines.
+        :arg stdout: where to write the output [default: ``sys.stdout``]
+        :arg stderr: where to write the error output [default: ``sys.stderr``]
+        """
         self._width = width
         self._stdout = stdout
         self._stderr = stderr
 
     @property
     def width(self):
+        """
+        Maximum number of characters in one line.
+        """
         return self._width
 
     def init(self):
+        """
+        Initializes the console. 
+        This method grabs the control of warnings and logging. 
+        All warnings or log message are redirected to the console.
+        """
         # Redirect warnings
         def showwarning(message, category, filename, lineno, file=None, line=None):
             name = category.__name__.replace('Warning', '')
@@ -133,10 +203,25 @@ class _Console(object):
         logger.addHandler(handler)
 
     def _pre_print(self, stream, text, color):
+        """
+        Performs pre-processing before the method : meth:`_print` is called.
+        """
         pass
 
     def _print(self, stream, text, color=COLOR_DEFAULT,
                eol=True, wrap=True, fill=True):
+        """
+        Prints a text.
+        
+        :arg stream: where to write the text (usually, stdout or stderr)
+        :arg text: text to print
+        :arg color: color of the text [default: COLOR_DEFAULT]
+        :arg eol: whether to add a carriage return after writing the text
+            [default: ``True``]
+        :arg wrap: whether to wrap the text [default: ``True``]
+        :arg fill: whether to add whitespace at the end of each line up to
+            the console's width [default: ``True``]
+        """
         self._pre_print(stream, text, color)
 
         # Wrap
@@ -162,31 +247,74 @@ class _Console(object):
         stream.flush()
 
     def _post_print(self, stream, text, color):
+        """
+        Performs post-processing after the method : meth:`_print` was called.
+        """
         pass
 
     def print_error(self, text, exit=True):
+        """
+        Prints an error message in red.
+        
+        :arg exit: whether to exit the program after writing the error message
+            [default: ``True``]
+        """
         self._print(self._stderr, 'Error: ' + text, color=COLOR_RED)
         if exit: self.close(1)
 
     def print_warn(self, text):
+        """
+        Prints a warning message in yellow.
+        """
         self._print(self._stderr, 'Warning: ' + text, color=COLOR_YELLOW)
 
     def print_info(self, text):
+        """
+        Prints a information message.
+        """
         self._print(self._stderr, 'Info: ' + text, color=COLOR_DEFAULT)
 
     def print_debug(self, text):
+        """
+        Prints a debug message in blue.
+        """
         self._print(self._stderr, 'Debug: ' + text, color=COLOR_BLUE)
 
     def print_message(self, text):
+        """
+        Prints a message.
+        """
         self._print(self._stdout, text, color=COLOR_DEFAULT)
 
     def print_success(self, text):
+        """
+        Prints a successful message in green.
+        """
         self._print(self._stdout, text, color=COLOR_GREEN)
 
     def print_line(self):
+        """
+        Prints a separating line.
+        """
         self._print(self._stdout, "-" * self._width)
 
     def _prompt(self, question, default=None, validators=[]):
+        """
+        Prints a question and retrieve the answer.
+        If no answer is given and a default value is given, the default value
+        is returned.
+        Otherwise, an empty string is returned.
+        
+        The answer can be validated with validators.
+        A validator is a function taking the answer as its only argument.
+        If the answer is invalid, the validator should raise an exception.
+        In this situation, the question will be asked again until a valid
+        answer is given.
+        
+        :arg question: question to ask
+        :arg default: default value when no answer is given
+        :arg validators: :class:`list` of functions
+        """
         text = self._PROMPT_PREFIX + question
         if default: text += ' [%s]' % default
         text += ": "
@@ -210,6 +338,14 @@ class _Console(object):
         return answer
 
     def prompt_text(self, question, default=None, validators=[]):
+        """
+        Prompts for a textual value. 
+        Any answer is accepted, except an empty string.
+        
+        :arg question: question to ask
+        :arg default: default value when no answer is given
+        :arg validators: :class:`list` of functions
+        """
         # Validators
         def _nonempty(answer):
             if not answer:
@@ -220,6 +356,18 @@ class _Console(object):
         return self._prompt(question, default, validators)
 
     def prompt_boolean(self, question, default=None, validators=[]):
+        """
+        Prompts for a boolean value. 
+        Only the following answers are accepted: ``y``, ``yes``, ``n``, ``no``
+        and their uppercase equivalents.
+        
+        :arg question: question to ask
+        :arg default: default value when no answer is given (either ``True`` or
+            ``False``)
+        :arg validators: :class:`list` of functions
+        
+        :rtype: :class:`bool`
+        """
         # Validators
         def _boolean(answer):
             if answer.upper() not in ('Y', 'YES', 'N', 'NO'):
@@ -235,6 +383,18 @@ class _Console(object):
         return answer.upper() in ('Y', 'YES')
 
     def prompt_file(self, question, default=None, should_exist=True, validators=[]):
+        """
+        Prompts for a path to a file.
+        If the ``should_exist`` flag is ``True``, the answer is valid if the
+        specified file exists.
+        Otherwise, any non-empty answer is accepted. 
+        
+        :arg question: question to ask
+        :arg default: default path when no answer is given
+        :arg should_exist: whether to check if the specified path is a valid
+            path to a file [default: ``True``]
+        :arg validators: :class:`list` of functions
+        """
         # Validators
         def _exists(answer):
             if should_exist and not os.path.isfile(answer):
@@ -247,6 +407,18 @@ class _Console(object):
         return os.path.abspath(answer)
 
     def prompt_directory(self, question, default=None, should_exist=True, validators=[]):
+        """
+        Prompts for a path to a directory.
+        If the ``should_exist`` flag is ``True``, the answer is valid if the
+        specified directory exists.
+        Otherwise, any non-empty answer is accepted. 
+        
+        :arg question: question to ask
+        :arg default: default path when no answer is given
+        :arg should_exist: whether to check if the specified path is a valid
+            path to a directory [default: ``True``]
+        :arg validators: :class:`list` of functions
+        """
         # Validators
         def _exists(answer):
             if should_exist and not os.path.isdir(answer):
@@ -259,6 +431,16 @@ class _Console(object):
         return os.path.abspath(answer)
 
     def prompt_float(self, question, default=None, validators=[]):
+        """
+        Prompts for a float value. 
+        An answer is valid if it can be converted to a float.
+        
+        :arg question: question to ask
+        :arg default: default value when no answer is given 
+        :arg validators: :class:`list` of functions
+        
+        :rtype: :class:`float`
+        """
         # Validators
         def _float(answer):
             try:
@@ -273,6 +455,16 @@ class _Console(object):
         return float(self.prompt_text(question, default, validators))
 
     def prompt_int(self, question, default=None, validators=[]):
+        """
+        Prompts for a integer value. 
+        An answer is valid if it can be converted to a integer.
+        
+        :arg question: question to ask
+        :arg default: default value when no answer is given 
+        :arg validators: :class:`list` of functions
+        
+        :rtype: :class:`int`
+        """
         # Validators
         def _int(answer):
             try:
@@ -287,6 +479,12 @@ class _Console(object):
         return int(self.prompt_text(question, default, validators))
 
     def close(self, retcode=0):
+        """
+        Closes the console and the program.
+        It resets the logging and warnings settings to the default settings.
+        
+        :arg retcode: return code sent to ``sys.exit``
+        """
         # Reset warnings
         warnings.showwarning = warnings._show_warning
 
@@ -298,6 +496,10 @@ class _Console(object):
         sys.exit(retcode)
 
 class UnixConsole(_Console):
+    """
+    Console for UNIX-like platform.
+    """
+
     _COLORS = {COLOR_DEFAULT: 0, COLOR_RED: 31, COLOR_GREEN: 32,
                COLOR_BLUE: 34, COLOR_YELLOW: 33, COLOR_PURPLE: 35}
 
@@ -311,6 +513,10 @@ if platform.system() == 'Windows':
     from ctypes import windll, c_ulong
 
     class WindowsConsole(_Console):
+        """
+        Console for Windows platform.
+        """
+
         _COLORS = {COLOR_DEFAULT: 15, COLOR_RED: 12, COLOR_GREEN: 10,
                    COLOR_BLUE: 9, COLOR_YELLOW: 14, COLOR_PURPLE: 15}
         #FIXME: Find purple color in Windows
@@ -332,6 +538,15 @@ if platform.system() == 'Windows':
             _Console.close(self)
 
 def create_console(width=80, stdout=sys.stdout, stderr=sys.stderr):
+    """
+    Creates a console based on the platform.
+    
+    :arg width: number of characters in one line [default: 80]
+        Text exceeding this number of characters will be wrapped to multiple
+        lines.
+    :arg stdout: where to write the output [default: ``sys.stdout``]
+    :arg stderr: where to write the error output [default: ``sys.stderr``]
+    """
     if platform.system() == 'Windows':
         return WindowsConsole(width, stdout, stderr)
     else:
