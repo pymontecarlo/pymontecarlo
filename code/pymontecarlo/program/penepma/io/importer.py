c819a16ecfa4fd5b5b9639966fe65fa2a66264dd
Philippe Pinard <philippe.pinard@gmail.com>
2012-05-22 17:02:32 +0200

    Import prz, photon spectrum, photon intensity, electron fraction and time from PENEPMA results.

--- code/pymontecarlo/program/penepma/io/importer.py
+++ code/pymontecarlo/program/penepma/io/importer.py
@@ -20,52 +20,42 @@ __license__ = "GPL v3"
 
 # Standard library modules.
 import os
+import glob
+from itertools import izip
+from collections import defaultdict
 
 # Third party modules.
 
 # Local modules.
-from pymontecarlo.io.importer import ImporterException
-from pymontecarlo.program.penelope.io.importer import Importer as _Importer
 from pymontecarlo.output.result import \
     (
     PhotonIntensityResult,
+    PhotonSpectrumResult,
     ElectronFractionResult,
     TimeResult,
+    PhiRhoZResult,
     create_intensity_dict,
+    create_phirhoz_dict,
     )
 from pymontecarlo.input.detector import \
     (
+     _PhotonDelimitedDetector,
 #     BackscatteredElectronEnergyDetector,
 #     BackscatteredElectronPolarAngularDetector,
-#     PhiRhoZDetector,
-#     PhotonIntensityDetector,
+     PhiRhoZDetector,
      PhotonSpectrumDetector,
+     PhotonIntensityDetector,
      ElectronFractionDetector,
      TimeDetector,
      )
 from pymontecarlo.util.transition import Transition
+from pymontecarlo.program.penelope.io.importer import \
+    Importer as _Importer, ImporterException
+from pymontecarlo.program.penepma.input.detector import index_delimited_detectors
 
 # Globals and constants variables.
 from pymontecarlo.output.result import \
     EMITTED, NOFLUORESCENCE, CHARACTERISTIC, BREMSSTRAHLUNG, TOTAL
-
-def _read_intensities_line(line):
-        values = line.split()
-
-        try:
-            transition = Transition(z=int(values[0]),
-                                    src=int(values[2]),
-                                    dest=int(values[1]))
-        except ValueError: # transition not supported
-            return None, 0.0, 0.0, 0.0, 0.0
-
-        nf = float(values[4]), float(values[5])
-        cf = float(values[6]), float(values[7])
-        bf = float(values[8]), float(values[9])
-        #tf = float(values[10]), float(values[11])
-        t = float(values[12]), float(values[13])
-
-        return transition, cf, bf, nf, t
 
 class Importer(_Importer):
 
@@ -74,14 +64,98 @@ class Importer(_Importer):
 
         self._detector_importers[PhotonSpectrumDetector] = \
             self._detector_photon_spectrum
-#        self._detector_importers[ElectronFractionDetector] = \
-#            self._detector_electron_fraction
-#        self._detector_importers[TimeDetector] = self._detector_time
+        self._detector_importers[PhotonIntensityDetector] = \
+            self._detector_photon_intensity
+        self._detector_importers[PhiRhoZDetector] = \
+            self._detector_phirhoz
+        self._detector_importers[ElectronFractionDetector] = \
+            self._detector_electron_fraction
+        self._detector_importers[TimeDetector] = self._detector_time
 
-    def _detector_photon_spectrum(self, options, key, detector, path):
-        # Find detector index
-        keys = sorted(options.detectors.findall(PhotonSpectrumDetector).keys())
-        index = keys.index(key) + 1
+    def _import_results(self, options, path, *args):
+        # Find index for each delimited detector
+        # The same method (index_delimited_detectors) is called when exporting
+        # the result. It ensures that the same index is used for all detectors
+        dets = options.detectors.findall(_PhotonDelimitedDetector)
+        phdets_key_index, phdets_index_keys = index_delimited_detectors(dets)
+
+        return _Importer._import_results(self, options, path,
+                                         phdets_key_index, phdets_index_keys,
+                                         *args)
+
+    def _detector_photon_spectrum(self, options, key, detector, path,
+                                  phdets_key_index, phdets_index_keys, *args):
+        index = phdets_key_index[key] + 1
+
+        # Find data files
+        charact_filepath = os.path.join(path, 'pe-charact-%s.dat' % str(index).zfill(2))
+        if not os.path.exists(charact_filepath):
+            raise ImporterException, "Data file %s cannot be found" % charact_filepath
+
+        spect_filepath = os.path.join(path, 'pe-spect-%s.dat' % str(index).zfill(2))
+        if not os.path.exists(spect_filepath):
+            raise ImporterException, "Data file %s cannot be found" % spect_filepath
+
+        # Load characteristic spectrum
+        charact = []
+        charact_unc = []
+
+        with open(charact_filepath, 'r') as fp:
+            for line in fp:
+                line = line.strip()
+                if line.startswith('#'): continue
+                values = line.split()
+
+                charact.append(float(values[1]))
+                charact_unc.append(float(values[2]))
+
+        # Load total spectrum
+        energies = []
+        total = []
+        total_unc = []
+
+        with open(spect_filepath, 'r') as fp:
+            for line in fp:
+                line = line.strip()
+                if line.startswith('#'): continue
+                values = line.split()
+
+                energies.append(float(values[0]))
+                total.append(float(values[1]))
+                total_unc.append(float(values[2]))
+
+        # Calculate background
+        background = [t - c for t, c in izip(total, charact)]
+        background_unc = [t + c for t, c in izip(total_unc, charact_unc)]
+
+        # Process
+        energy_channel_width_eV = energies[1] - energies[0]
+        energy_offset_eV = energies[0] - energy_channel_width_eV / 2.0
+
+        return PhotonSpectrumResult(energy_offset_eV, energy_channel_width_eV,
+                                    total, total_unc, background, background_unc)
+
+    def _detector_photon_intensity(self, options, key, detector, path,
+                                   phdets_key_index, phdets_index_keys, *args):
+        def _read_intensities_line(line):
+            values = line.split()
+
+            try:
+                transition = Transition(z=int(values[0]),
+                                        src=int(values[2]),
+                                        dest=int(values[1]))
+            except ValueError: # transition not supported
+                return None, 0.0, 0.0, 0.0, 0.0
+
+            nf = float(values[4]), float(values[5])
+            cf = float(values[6]), float(values[7])
+            bf = float(values[8]), float(values[9])
+            #tf = float(values[10]), float(values[11]) # skip not needed
+            t = float(values[12]), float(values[13])
+
+            return transition, cf, bf, nf, t
+
+        index = phdets_key_index[key] + 1
 
         # Find data files
         emitted_filepath = os.path.join(path, 'pe-inten-%s.dat' % str(index).zfill(2))
@@ -124,8 +198,100 @@ class Importer(_Importer):
 
         return PhotonIntensityResult(intensities)
 
-    def _detector_electron_fraction(self, options, key, detector, path):
-        pass
+    def _detector_phirhoz(self, options, key, detector, path,
+                          phdets_key_index, phdets_index_keys, *args):
+        index = phdets_key_index[key] + 1
 
-    def _detector_time(self, options, key, detector, path):
-        pass
+        # Find data files
+        pattern = os.path.join(path, 'pe-prz-*-%s.dat' % str(index).zfill(2))
+        filepaths = glob.glob(pattern)
+
+        # Load distributions
+        distributions = {}
+
+        for filepath in filepaths:
+            # Transition
+            code = os.path.basename(filepath)[7:13]
+            z = int(code[:2])
+            dest = int(code[2:4])
+            src = int(code[4:6])
+            transition = Transition(z, src, dest)
+
+            # Read data
+            data = defaultdict(list)
+            with open(filepath, 'r') as fp:
+                for line in fp:
+                    line = line.strip()
+                    if line.startswith('#'): continue
+                    values = line.split()
+
+                    data['zs'].append(float(values[0]))
+                    data['zfs'].append(float(values[1]))
+                    data['gnf'].append(float(values[4]))
+                    data['gnf_unc'].append(float(values[5]))
+                    data['gt'].append(float(values[20]))
+                    data['gt_unc'].append(float(values[21]))
+                    data['enf'].append(float(values[2]))
+                    data['enf_unc'].append(float(values[3]))
+                    data['et'].append(float(values[18]))
+                    data['et_unc'].append(float(values[19]))
+
+            # Convert units of depth
+            data['zs'] = [-z * 1e-2 for z in data['zs']]
+            data['zfs'] = [-z * 1e-2 for z in data['zfs']]
+
+            # Reverse all data to have increase z values
+            for value in data.itervalues():
+                value.reverse()
+
+            # Assemble data
+            gnf = data['zs'], data['gnf'], data['gnf_unc']
+            gt = data['zfs'], data['gt'], data['gt_unc']
+            enf = data['zs'], data['enf'], data['enf_unc']
+            et = data['zfs'], data['et'], data['et_unc']
+
+            distributions.update(create_phirhoz_dict(transition, gnf, gt, enf, et))
+
+        return PhiRhoZResult(distributions)
+
+    def _read_log(self, path):
+        """
+        Returns the last line of the :file:`penepma.csv` log file as a 
+        :class:`dict` where the keys are the header of each column and the 
+        values are the values of the last line.
+        
+        :arg path: directory containing the simulation files
+        """
+        filepath = os.path.join(path, 'penepma.csv')
+        if not os.path.exists(filepath):
+            raise ImporterException, "Data file %s cannot be found" % filepath
+
+        with open(filepath, 'r') as fp:
+            header = fp.readline().strip().split(',')
+            header = [s.strip() for s in header]
+            line = []
+
+            for line in fp:
+                continue
+
+        line = line.strip().rstrip(',').split(',')
+        values = map(float, line)
+
+        return dict(zip(header, values))
+
+    def _detector_electron_fraction(self, options, key, detector, path, *args):
+        line = self._read_log(path)
+
+        absorbed = line['F_ABS'], line['F_ABS_E']
+        backscattered = line['F_BSE'], line['F_BSE_E']
+        transmitted = line['F_TRANS'], line['F_TRANS_E']
+
+        return ElectronFractionResult(absorbed, backscattered, transmitted)
+
+    def _detector_time(self, options, key, detector, path, *args):
+        line = self._read_log(path)
+
+        simulation_time_s = line['SIM_TIME']
+        simulation_speed_s = 1.0 / line['SIM_SPEED'], 0.0
+
+        return TimeResult(simulation_time_s, simulation_speed_s)
--- code/pymontecarlo/program/penepma/io/test_importer.py
+++ code/pymontecarlo/program/penepma/io/test_importer.py
@@ -20,7 +20,12 @@ from math import radians
 from pymontecarlo.testcase import TestCase
 
 from pymontecarlo.input.options import Options
-from pymontecarlo.input.detector import PhotonSpectrumDetector
+from pymontecarlo.input.detector import \
+    (PhotonSpectrumDetector,
+     PhotonIntensityDetector,
+     PhiRhoZDetector,
+     ElectronFractionDetector,
+     TimeDetector)
 from pymontecarlo.program.penepma.io.importer import Importer
 
 import DrixUtilities.Files as Files
@@ -43,16 +48,14 @@ class TestImporter(TestCase):
     def testskeleton(self):
         self.assertTrue(True)
 
-    def test_detector_photon_spectrum(self):
+    def test_detector_photon_intensity(self):
         # Create
         ops = Options(name='test1')
         ops.beam.energy_eV = 20e3
         ops.detectors['xray1'] = \
-            PhotonSpectrumDetector((radians(35), radians(45)), (0, radians(360.0)),
-                                   (0, 20e3), 1000)
+            PhotonIntensityDetector((radians(35), radians(45)), (0, radians(360.0)))
         ops.detectors['xray2'] = \
-            PhotonSpectrumDetector((radians(-35), radians(-45)), (0, radians(360.0)),
-                                   (0, 20e3), 1000)
+            PhotonIntensityDetector((radians(-35), radians(-45)), (0, radians(360.0)))
 
         # Import
         results = self.i.import_from_dir(ops, self.testdata)
@@ -60,7 +63,7 @@ class TestImporter(TestCase):
         # Test
         self.assertEqual(2, len(results))
 
-        result = results['xray1']
+        result = results['xray2']
 
         val, unc = result.intensity('W Ma1')
         self.assertAlmostEqual(6.07152e-05, val, 9)
@@ -77,6 +80,131 @@ class TestImporter(TestCase):
         val, unc = result.intensity('W Ma1', absorption=False, fluorescence=False)
         self.assertAlmostEqual(4.883132e-4, val, 9)
         self.assertAlmostEqual(4.45e-06, unc, 9)
+
+    def test_detector_photon_spectrum(self):
+        # Create
+        ops = Options(name='test1')
+        ops.beam.energy_eV = 20e3
+        ops.detectors['spectrum'] = \
+            PhotonSpectrumDetector((radians(35), radians(45)), (0, radians(360.0)),
+                                   (0, 20e3), 1000)
+
+        # Import
+        results = self.i.import_from_dir(ops, self.testdata)
+
+        # Test
+        self.assertEqual(1, len(results))
+
+        result = results['spectrum']
+
+        energies, values, uncs = result.get_total()
+        self.assertEqual(1000, len(energies))
+        self.assertEqual(1000, len(values))
+        self.assertEqual(1000, len(uncs))
+        self.assertAlmostEqual(0.0, energies[0], 4)
+        self.assertAlmostEqual(19980.0, energies[-1], 4)
+        self.assertAlmostEqual(2.841637e-6, values[31], 10)
+        self.assertAlmostEqual(8.402574e-6, uncs[31], 10)
+
+        energies, values, uncs = result.get_background()
+        self.assertEqual(1000, len(energies))
+        self.assertEqual(1000, len(values))
+        self.assertEqual(1000, len(uncs))
+        self.assertAlmostEqual(0.0, energies[0], 4)
+        self.assertAlmostEqual(19980.0, energies[-1], 4)
+        self.assertAlmostEqual(4.0148e-8, values[31], 10)
+        self.assertAlmostEqual(1.6802574e-5, uncs[31], 10)
+
+    def test_detector_phirhoz(self):
+        # Create
+        ops = Options(name='test1')
+        ops.beam.energy_eV = 20e3
+        ops.detectors['prz'] = \
+            PhiRhoZDetector((radians(35), radians(45)), (0, radians(360.0)), 500)
+
+        # Import
+        results = self.i.import_from_dir(ops, self.testdata)
+
+        # Test
+        self.assertEqual(1, len(results))
+
+        result = results['prz']
+
+        self.assertTrue(result.exists('Al Ka1'))
+        self.assertTrue(result.exists('Fe Ka1'))
+
+        zs, vals, uncs = result.get('Al Ka1')
+        self.assertEqual(249, len(zs))
+        self.assertEqual(249, len(vals))
+        self.assertEqual(249, len(uncs))
+        self.assertAlmostEqual(0.0, zs[-1], 4)
+        self.assertAlmostEqual(4.704016e-3, vals[-1], 6)
+        self.assertAlmostEqual(7.06e-3, uncs[-1], 6)
+
+        zs, vals, uncs = result.get('Al Ka1', absorption=False)
+        self.assertEqual(249, len(zs))
+        self.assertEqual(249, len(vals))
+        self.assertEqual(249, len(uncs))
+        self.assertAlmostEqual(0.0, zs[-1], 4)
+        self.assertAlmostEqual(3.910867e-3, vals[-1], 6)
+        self.assertAlmostEqual(2.35e-3, uncs[-1], 6)
+
+        zs, vals, uncs = result.get('Al Ka1', fluorescence=False)
+        self.assertEqual(249, len(zs))
+        self.assertEqual(249, len(vals))
+        self.assertEqual(249, len(uncs))
+        self.assertAlmostEqual(0.0, zs[-1], 4)
+        self.assertAlmostEqual(1.353710, vals[-1], 6)
+        self.assertAlmostEqual(3.63e+1, uncs[-1], 6)
+
+        zs, vals, uncs = result.get('Al Ka1', absorption=False, fluorescence=False)
+        self.assertEqual(249, len(zs))
+        self.assertEqual(249, len(vals))
+        self.assertEqual(249, len(uncs))
+        self.assertAlmostEqual(0.0, zs[-1], 4)
+        self.assertAlmostEqual(1.764721, vals[-1], 6)
+        self.assertAlmostEqual(7.56e-1, uncs[-1], 6)
+
+    def test_detector_electron_fraction(self):
+        # Create
+        ops = Options(name='test1')
+        ops.beam.energy_eV = 20e3
+        ops.detectors['fraction'] = ElectronFractionDetector()
+
+        # Import
+        results = self.i.import_from_dir(ops, self.testdata)
+
+        # Test
+        self.assertEqual(1, len(results))
+
+        result = results['fraction']
+
+        self.assertAlmostEqual(0.2336, result.backscattered[0], 4)
+        self.assertAlmostEqual(0.6713e-2, result.backscattered[1], 6)
+
+        self.assertAlmostEqual(0.0, result.transmitted[0], 4)
+        self.assertAlmostEqual(0.0, result.transmitted[1], 4)
+
+        self.assertAlmostEqual(0.7759, result.absorbed[0], 4)
+        self.assertAlmostEqual(0.5447e-2, result.absorbed[1], 6)
+
+    def test_detector_time(self):
+        # Create
+        ops = Options(name='test1')
+        ops.beam.energy_eV = 20e3
+        ops.detectors['time'] = TimeDetector()
+
+        # Import
+        results = self.i.import_from_dir(ops, self.testdata)
+
+        # Test
+        self.assertEqual(1, len(results))
+
+        result = results['time']
+
+        self.assertAlmostEqual(0.3495e3, result.simulation_time_s, 4)
+        self.assertAlmostEqual(1.0 / 0.1508e3, result.simulation_speed_s[0], 4)
+
 
 if __name__ == '__main__': #pragma: no cover
     logging.getLogger().setLevel(logging.DEBUG)
