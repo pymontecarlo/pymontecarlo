377e3c7449402aebb60f00c956406edeb5cb74b3
hg <devnull@localhost>
2012-10-23 22:06:21 +0200

    Add new module to track progress of any task. The module works similarly to logging.

---
+++ code/pymontecarlo/util/progress.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+"""
+================================================================================
+:mod:`progress` -- Track progress of task(s)
+================================================================================
+
+.. module:: progress
+   :synopsis: Track progress of task
+
+.. inheritance-diagram:: pymontecarlo.util.progress
+
+"""
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2012 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import threading
+from operator import attrgetter
+
+# Third party modules.
+
+# Local modules.
+
+# Globals and constants variables.
+
+class Task:
+
+    def __init__(self, alias):
+        """
+        Creates a new task.
+        
+        :arg alias: alias of task
+        """
+        self._alias = str(alias)
+        self._progress = 0.0
+        self._status = ''
+        self._lock = threading.RLock()
+
+    def __repr__(self):
+        return '<Task(%s)>' % self._alias
+
+    def __str__(self):
+        return self._alias
+
+    def __cmp__(self, other):
+        return cmp(self.progress, other.progress)
+
+    @property
+    def progress(self):
+        """
+        Returns/set the progress of this task.
+        The progress is a value between 0.0 and 1.0, inclusively.
+        """
+        return self._progress
+
+    @progress.setter
+    def progress(self, value):
+        self._lock.acquire()
+        try:
+            if value < 0: value = 0.0
+            if value > 1: value = 1.0
+            self._progress = float(value)
+        finally:
+            self._lock.release()
+
+    @property
+    def status(self):
+        """
+        Returns/sets the status of this task.
+        The status is a string.
+        """
+        return self._status
+
+    @status.setter
+    def status(self, text):
+        self._lock.acquire()
+        try:
+            if text is None: text = ''
+            self._status = str(text)
+        finally:
+            self._lock.release()
+
+_PROGRESS_GETTER = attrgetter('progress')
+
+class ProgressTracker:
+
+    def __init__(self):
+        self._tasks = []
+        self._lock = threading.RLock()
+
+    def start_task(self, alias=None):
+        """
+        Starts a new task.
+        
+        :arg alias: alias of the task. If ``None``, the task is given a name based
+            on its creation order.
+        :type alias: :class:`str`
+        """
+        self._lock.acquire()
+        try:
+            if not alias: alias = 'Task %i' % len(self._tasks)
+            task = Task(alias)
+            self._tasks.append(task)
+        finally:
+            self._lock.release()
+        return task
+
+    def stop_task(self, task):
+        """
+        Stops the specified task.
+        If the stops was previously stopped, no exception is raised.
+        A task cannot be started again after being stopped.
+        """
+        self._lock.acquire()
+        try:
+            self._tasks.remove(task)
+        except:
+            pass
+        finally:
+            self._lock.release()
+
+    def stop_all(self):
+        """
+        Stops all running tasks.
+        """
+        self._lock.acquire()
+        try:
+            self._tasks = []
+        finally:
+            self._lock.release()
+
+    def is_running(self):
+        """
+        Returns whether at least one task is alive.
+        """
+        self._lock.acquire()
+        try:
+            return len(self._tasks) > 0
+        finally:
+            self._lock.release()
+
+    def progress(self):
+        """
+        Returns the progress of all the tasks.
+        The progress is a value between 0.0 and 1.0, inclusively.
+        If more than one task is running, the average progress is returned.
+        """
+        self._lock.acquire()
+        try:
+            if not self._tasks: return 0.0
+            return sum(map(_PROGRESS_GETTER, self._tasks)) / len(self._tasks)
+        finally:
+            self._lock.release()
+        return progress
+
+    def status(self):
+        """
+        Returns the status of the task with the highest progress.
+        """
+        self._lock.acquire()
+        try:
+            if not self._tasks: return ''
+            return sorted(self._tasks, reverse=True)[0].status
+        finally:
+            self._lock.release()
+
+root = ProgressTracker()
+
+def start_task(alias=None):
+    """
+    Starts a new task.
+    
+    :arg alias: alias of the task. If ``None``, the task is given a name based
+        on its creation order.
+    :type alias: :class:`str`
+    """
+    return root.start_task(alias)
+
+def stop_task(task):
+    """
+    Stops the specified task.
+    If the stops was previously stopped, no exception is raised.
+    A task cannot be started again after being stopped.
+    """
+    root.stop_task(task)
+
+def stop_all():
+    """
+    Stops all running tasks.
+    """
+    root.stop_all()
+
+def is_running():
+    """
+    Returns whether at least one task is alive.
+    """
+    return root.is_running()
+
+def progress():
+    """
+    Returns the progress of all the tasks.
+    The progress is a value between 0.0 and 1.0, inclusively.
+    If more than one task is running, the average progress is returned.
+    """
+    return root.progress()
+
+def status():
+    """
+    Returns the status of the task with the highest progress.
+    """
+    return root.status()
