609b83a910be9c9435b830f2703c2503f7db4ee4
Philippe Pinard <philippe.pinard@gmail.com>
2013-03-03 18:01:34 +0100

    Better handling of dependencies. Fix problem with develop uninstall.

--- pavement.py
+++ pavement.py
@@ -38,14 +38,50 @@ def _call_setup(filepath, *args):
     print '--Running %s' % os.path.relpath(filepath, os.curdir)
     check_call(process_args, cwd=cwd)
 
-def _call_all_setups(*args):
+def _call_core_setup(*args):
     """
-    Calls setup.py of pymontecarlo and all programs.
+    Calls setup.py of core.
     """
     _call_setup(os.path.join('src', 'core', 'setup.py'), *args)
 
+def _call_program_setups(*args):
+    """
+    Calls setup.py of all programs.
+    """
     for setup_path in glob.glob(os.path.join('src', 'programs', '*', 'setup.py')):
         _call_setup(setup_path, *args)
+
+def _call_dependency_setups(*args):
+    """
+    Calls setup.py of all dependencies.
+    """
+    # casinoTools
+    projectdir = config.get('projects', 'casinoTools')
+    filepath = os.path.join(projectdir, 'setup_casino2.py')
+    _call_setup(filepath, *args)
+
+    # winxrayTools
+    projectdir = config.get('projects', 'winxrayTools')
+    filepath = os.path.join(projectdir, 'setup.py')
+    _call_setup(filepath, *args)
+
+    # PouchouPichoirModels
+    projectdir = config.get('projects', 'PouchouPichoirModels')
+    filepath = os.path.join(projectdir, 'setup.py')
+    _call_setup(filepath, *args)
+
+    # PENELOPE
+    projectdir = config.get('projects', 'penelope')
+    filepath = os.path.join(projectdir, 'setup.py')
+    _call_setup(filepath, *args)
+
+def _call_all_setups(*args):
+    """
+    Calls setup.py of pymontecarlo core and all programs.
+    """
+    _call_dependency_setups(*args)
+    _call_core_setup(*args)
+    _call_program_setups(*args)
 
 @task
 def clean():
@@ -59,38 +95,32 @@ def purge():
     """
     Cleans all build and dist output from pymontecarlo and programs.
     """
-    _call_all_setups('clean', '--purge')
+    # NOTE: Dependencies do not have purge option
+    _call_core_setup(*args)
+    _call_program_setups(*args)
 
 @task
-@cmdopts([('uninstall', 'u', 'Uninstall development mode')])
+@cmdopts([('uninstall', 'u', 'Uninstall development mode'),
+          ('prefix=', '', 'Prefix for installation')])
 def develop(options):
     """
     Registers all programs as development mode.
     """
-    args = ['-u'] if hasattr(options, 'uninstall') else []
+    args = []
+    uninstall = hasattr(options, 'uninstall')
+    if uninstall: args.append('-u')
+    if hasattr(options, 'prefix'): args.extend(['--prefix', options.prefix])
 
-    # casinoTools
-    projectdir = config.get('projects', 'casinoTools')
-    filepath = os.path.join(projectdir, 'setup_casino2.py')
-    _call_setup(filepath, 'develop', *args)
-
-    # winxrayTools
-    projectdir = config.get('projects', 'winxrayTools')
-    filepath = os.path.join(projectdir, 'setup.py')
-    _call_setup(filepath, 'develop', *args)
-
-    # PouchouPichoirModels
-    projectdir = config.get('projects', 'PouchouPichoirModels')
-    filepath = os.path.join(projectdir, 'setup.py')
-    _call_setup(filepath, 'develop', *args)
-
-    # PENELOPE
-    projectdir = config.get('projects', 'penelope')
-    filepath = os.path.join(projectdir, 'setup.py')
-    _call_setup(filepath, 'develop', *args)
+    # Dependencies
+    _call_dependency_setups('develop', *args)
 
     # Core and programs
-    _call_all_setups('develop', *args)
+    if not uninstall:
+        _call_core_setup('develop', *args)
+        _call_program_setups('develop', *args)
+    else:
+        _call_program_setups('develop', *args)
+        _call_core_setup('develop', *args)
 
 @task
 def sdist():
@@ -108,19 +138,9 @@ def bdist_egg():
     dist_dir = os.path.abspath(os.path.join(os.curdir, 'dist'))
     _call_all_setups('bdist_egg', '-d', dist_dir)
 
-    # casinoTools
-    projectdir = config.get('projects', 'casinoTools')
-    filepath = os.path.join(projectdir, 'setup_casino2.py')
-    _call_setup(filepath, 'bdist_egg', '-d', dist_dir)
-
-    # winxrayTools
-    projectdir = config.get('projects', 'winxrayTools')
-    filepath = os.path.join(projectdir, 'setup.py')
-    _call_setup(filepath, 'bdist_egg', '-d', dist_dir)
-
 @task
 def test():
     """
     Tests all
     """
-    _call_setup('setup.py', 'test')
+    _call_all_setups('test')
