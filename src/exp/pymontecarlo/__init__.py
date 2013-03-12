7e668d5e9b8fab4c7a25032ca1f30e08b29179bc
Philippe Pinard <philippe.pinard@gmail.com>
2013-03-12 19:45:42 +0100

    Move quant package to its own source directory (exp).

---
+++ src/exp/pymontecarlo/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+================================================================================
+:mod:`pymontecarlo` -- Common interface to several Monte Carlo codes
+================================================================================
+
+"""
+
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2011 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# This is required to create a namespace package.
+# A namespace package allows programs to be located in different directories or
+# eggs.
+
+__import__('pkg_resources').declare_namespace(__name__)
+
