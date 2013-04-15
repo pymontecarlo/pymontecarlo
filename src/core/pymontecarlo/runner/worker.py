8e2f6726a5d0a2bbd302248f0a59f4e7f153b313
Philippe Pinard <philippe.pinard@gmail.com>
2013-04-15 08:36:13 +0200

    Fix bug that _process variable was not instantiated.

--- src/core/pymontecarlo/runner/worker.py
+++ src/core/pymontecarlo/runner/worker.py
@@ -61,34 +61,6 @@ class Worker(object):
         """
         raise NotImplementedError
 
-#        """
-#        Creates all simulation files without starting the simulation(s).
-#        All simulations in the options queue will be created.
-#        The simulation files created will depend on the Monte Carlo program used.
-#        The simulation files could also be a folder.
-#        The simulation files are saved in the *output directory*.
-#        """
-#        while True:
-#            try:
-#                options = self._queue_options.get()
-#                self._create(options, self._outputdir)
-#                self._queue_options.task_done()
-#            except Exception:
-#                self.stop()
-#                self._queue_options.raise_exception()
-#
-#    def _create(self, options, dirpath):
-#        """
-#        Creates the simulation file(s) from the options and saves it inside the
-#        specified directory.
-#        This method should be implemented by derived class as it is specific
-#        to the different Monte Carlo programs.
-#
-#        :arg options: options of a simulation
-#        :arg dirpath: directory where to save the simulation file(s)
-#        """
-#        raise NotImplementedError
-
     def run(self, options, outputdir, workdir):
         """
         Creates and runs a simulation from the specified options.
@@ -120,6 +92,10 @@ class Worker(object):
         return self._progress, self._status
 
 class SubprocessWorker(Worker):
+
+    def __init__(self):
+        Worker.__init__(self)
+        self._process = None
 
     def reset(self):
         Worker.reset(self)
