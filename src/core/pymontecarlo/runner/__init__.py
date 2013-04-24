1d7a58f63cd365fa5ddd4cc126a46fdd83a39b64
Niklas Mevenkamp niklas mevenkamp <Niklas Mevenkamp niklas.mevenkamp@gmx.net>
2013-04-24 17:07:39 +0200

    Declare runner package as namespace.

---
+++ src/core/pymontecarlo/runner/__init__.py
@@ -0,0 +1,15 @@
+"""
+================================================================================
+:mod:`runner` -- Runner Monte Carlo programs
+================================================================================
+
+.. module:: runner
+   :synopsis: Runner Monte Carlo programs
+
+"""
+
+# This is required to create a namespace package.
+# A namespace package allows programs to be located in different directories or
+# eggs.
+
+__import__('pkg_resources').declare_namespace(__name__)
