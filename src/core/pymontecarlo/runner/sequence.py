cd94dc7b5fa9669271810261d48112e18670fa23
hg <devnull@localhost>
2013-04-24 14:06:37 +0200

    In SequenceRunner use options.uuid property to create a lookup.

--- src/core/pymontecarlo/runner/sequence.py
+++ src/core/pymontecarlo/runner/sequence.py
@@ -19,11 +19,14 @@ __copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
 __license__ = "GPL v3"
 
 # Standard library modules.
+import uuid
+from operator import attrgetter
 
 # Third party modules.
 
 # Local modules.
 from pymontecarlo.runner.base import _Runner
+from pymontecarlo.output.results import ResultsSequence
 
 # Globals and constants variables.
 
@@ -32,9 +35,13 @@ class SequenceRunner(_Runner):
     def __init__(self, runner):
         _Runner.__init__(self, runner.program)
         self._runner = runner
-
+        self._lookup = {}
+        
     def put(self, options_seq):
-        pass
+        options_seq_id = uuid.uuid4()
+        for pos, options in enumerate(options_seq):
+            self._lookup[options.uuid] = (options_seq_id, pos)        
+            self._runner.put(options)
 
     def start(self):
         self._runner.start()
@@ -52,8 +59,30 @@ class SequenceRunner(_Runner):
         self._runner.join()
 
     def get_results(self):
-        pass
-
+        list_results_seq = []
+        list_all_results = self._runner.get_results()
+        
+        # Create dictionary where results are sorted by sequence id
+        dict_results = {}
+        for results in list_all_results:
+            options_seq_id = self._lookup[results.options.uuid][0]
+            if not options_seq_id in dict_results:
+                dict_results[options_seq_id] = {}
+            else:
+                dict_results[options_seq_id].append(results)
+        
+        # Create ResultsSequence objects by sorting the lists in the dictionary
+        for list_results in dict_results:
+            pos = lambda x: self._lookup[x.options.uuid][1]
+            list_results = sorted(list_results, key=pos)
+            list_results_seq.append(ResultsSequence(list_results))
+        
+        # Delete all used options from the lookup
+        for results in list_all_results:
+            del self._lookup[results.options.uuid]
+                
+        return list_results_seq
+    
     def report(self):
         return self._runner.report()
 
