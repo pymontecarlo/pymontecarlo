41730c8aa5962de87bf7d76a3bdbace1daf8dbbb
Philippe Pinard <philippe.pinard@gmail.com>
2013-06-27 08:15:52 +0200

    New module to save and load object from XML.

---
+++ src/core/pymontecarlo/util/test_xmlmapper.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+""" """
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import unittest
+import logging
+import xml.etree.ElementTree as ElementTree
+
+# Third party modules.
+
+# Local modules.
+from pymontecarlo.util.xmlmapper import XMLMapper, Element, Attribute, PythonType, UserType
+
+# Globals and constants variables.
+
+mapper = XMLMapper()
+
+class Human(object):
+
+    def __init__(self, firstname, lastname, age,
+                 middlenames=None, luckynumbers=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        if middlenames is None: middlenames = []
+        self.middlenames = middlenames
+        if luckynumbers is None: luckynumbers = []
+        self.luckynumbers = luckynumbers
+
+    def __repr__(self):
+        return '<Human(%s %s aged %i)>' % (self.firstname, self.lastname, self.age)
+
+mapper.register(Human, 'human',
+                Attribute('firstname', PythonType(str)),
+                Attribute('lastname', PythonType(str)),
+                Attribute('middlenames', PythonType(str), iterable=True),
+                Attribute('age', PythonType(int)),
+                Element('luckynumbers', PythonType(int), iterable=True))
+
+class Family(object):
+    
+    def __init__(self, father, mother, status='Married'):
+        self.father = father
+        self.mother = mother
+        self.status = status
+        self.children = []
+
+mapper.register(Family, 'family',
+                Element('father', UserType(Human)),
+                Element('mother', UserType(Human)),
+                Element('status', PythonType(str)),
+                Element('children', UserType(Human), iterable=True))
+
+class TestXMLMapper(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+        father = Human('John', 'Smith', 52)
+        mother = Human('Mary', 'Smith', 53)
+        self.family = Family(father, mother)
+
+        self.family.children.append(Human('Steve', 'Smith', 5))
+        self.family.children.append(Human('Sarah', 'Smith', 9))
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testto_xml1(self):
+        element = mapper.to_xml(Human('Steve', 'Maclean', 5, luckynumbers=[45, 68, 78]))
+
+        self.assertEqual('Steve', element.get('firstname'))
+        self.assertEqual('Maclean', element.get('lastname'))
+        self.assertEqual('5', element.get('age'))
+        self.assertEqual('45,68,78', element.find('luckynumbers').text)
+        self.assertIsNone(element.get('middlenames'))
+
+    def testto_xml2(self):
+        element = mapper.to_xml(self.family)
+        self.assertEqual(4, len(element.findall('*')))
+
+    def testfrom_xml1(self):
+        text = '<human age="5" firstname="Steve" lastname="Maclean" />'
+        element = ElementTree.fromstring(text)
+        obj = mapper.from_xml(element)
+
+        self.assertEqual('Steve', obj.firstname)
+        self.assertEqual('Maclean', obj.lastname)
+        self.assertEqual(5, obj.age)
+        self.assertEqual(0, len(obj.luckynumbers))
+        self.assertEqual(0, len(obj.middlenames))
+
+    def testfrom_xml2(self):
+        text = '''<human age="5" firstname="Steve" lastname="Maclean"
+                         middlenames="L,M,P">
+                      <luckynumbers>45,68,78</luckynumbers>
+                  </human>'''
+        element = ElementTree.fromstring(text)
+        obj = mapper.from_xml(element)
+
+        self.assertEqual('Steve', obj.firstname)
+        self.assertEqual('Maclean', obj.lastname)
+        self.assertEqual(5, obj.age)
+        self.assertEqual(3, len(obj.luckynumbers))
+        self.assertEqual(45, obj.luckynumbers[0])
+        self.assertEqual(68, obj.luckynumbers[1])
+        self.assertEqual(78, obj.luckynumbers[2])
+        self.assertEqual(3, len(obj.middlenames))
+        self.assertEqual('L', obj.middlenames[0])
+        self.assertEqual('M', obj.middlenames[1])
+        self.assertEqual('P', obj.middlenames[2])
+
+    def testfrom_xml3(self):
+        text = '''<family>
+                      <father>
+                          <human age="52" firstname="John" lastname="Smith" />
+                      </father>
+                      <mother>
+                          <human age="53" firstname="Mary" lastname="Smith" />
+                      </mother>
+                      <status>Married</status>
+                  </family>'''
+        element = ElementTree.fromstring(text)
+        obj = mapper.from_xml(element)
+
+        self.assertEqual('John', obj.father.firstname)
+        self.assertEqual('Smith', obj.father.lastname)
+        self.assertEqual(52, obj.father.age)
+
+        self.assertEqual('Mary', obj.mother.firstname)
+        self.assertEqual('Smith', obj.mother.lastname)
+        self.assertEqual(53, obj.mother.age)
+
+        self.assertEqual('Married', obj.status)
+
+if __name__ == '__main__': #pragma: no cover
+    logging.getLogger().setLevel(logging.DEBUG)
+    unittest.main()
---
+++ src/core/pymontecarlo/util/xmlmapper.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python
+"""
+================================================================================
+:mod:`xmlmapper` -- Mapper functions to XML
+================================================================================
+
+.. module:: xmlmapper
+   :synopsis: Mapper functions to XML
+
+.. inheritance-diagram:: pymontecarlo.util.xmlmapper
+
+"""
+
+# Script information for the file.
+__author__ = "Philippe T. Pinard"
+__email__ = "philippe.pinard@gmail.com"
+__version__ = "0.1"
+__copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
+__license__ = "GPL v3"
+
+# Standard library modules.
+import xml.etree.ElementTree as ElementTree
+from collections import Iterable
+
+# Third party modules.
+
+# Local modules.
+
+from pymontecarlo.util.manager import Manager
+
+# Globals and constants variables.
+
+class _XMLType(object):
+
+    def from_xml(self, value):
+        raise NotImplementedError
+
+    def to_xml(self, value):
+        raise NotImplementedError
+
+class PythonType(_XMLType):
+    
+    def __init__(self, type_):
+        self._type = type_
+
+    def from_xml(self, value):
+        return self._type(value)
+
+    def to_xml(self, value):
+        return str(value)
+    
+class UserType(_XMLType):
+    
+    def __init__(self, klass):
+        self._klass = klass
+        
+    def from_xml(self, value):
+        assert isinstance(value, self._klass)
+        return value
+    
+    def to_xml(self, value):
+        assert isinstance(value, self._klass)
+        return value
+
+class _XMLItem(object):
+    
+    def __init__(self, objattr, type_, xmlname=None, iterable=False, *args, **kwargs):
+        self.objattr = objattr
+        self.type_ = type_
+        self.xmlname = xmlname or objattr
+        self.iterable = iterable
+
+    def __repr__(self):
+        return '<%s(%s <-> %s)>' % (self.__class__.__name__,
+                                    self.objattr, self.xmlname)
+        
+    def _get_object_values(self, obj, manager):
+        values = getattr(obj, self.objattr)
+
+        if not self.iterable:
+            values = [values]
+        else:
+            if not isinstance(values, Iterable) or \
+                    manager.is_registered(klass=values.__class__):
+                values = [values]
+
+        return map(self.type_.to_xml, values)
+    
+    def _update_element(self, element, values, manager):
+        raise NotImplementedError
+
+    def dump(self, obj, element, manager):
+        values = self._get_object_values(obj, manager)
+        self._update_element(element, values, manager)
+        
+    def _extract_values(self, element, manager):
+        raise NotImplementedError
+        
+    def _set_object_values(self, obj, values):
+        values = map(self.type_.from_xml, values)
+
+        if not self.iterable:
+            values = values[0]
+
+        setattr(obj, self.objattr, values)
+
+    def load(self, obj, element, manager):
+        values = self._extract_values(element, manager)
+        self._set_object_values(obj, values)
+
+class Element(_XMLItem):
+    
+    def _update_element(self, element, values, manager):
+        subelement = ElementTree.Element(self.xmlname)
+
+        if isinstance(self.type_, UserType):
+            self._update_element_usertype(subelement, values, manager)
+        else:
+            self._update_element_nousertype(subelement, values, manager)
+
+        element.append(subelement)
+        
+    def _update_element_usertype(self, subelement, values, manager):
+        for value in values:
+            subelement.append(manager.to_xml(value))
+
+    def _update_element_nousertype(self, subelement, values, manager):
+        text = []
+        for value in values:
+            if ',' in str(value):
+                raise ValueError, "Value %s contains ','. Cannot be serialised" % value
+            text.append(value)
+        subelement.text = ','.join(text)
+
+    def _extract_values(self, element, manager):
+        subelement = element.find(self.xmlname)
+        if subelement is None:
+            return []
+
+        subsubelements = list(subelement)
+        if subsubelements:
+            values = self._extract_values_usertype(subelement, manager)
+        else:
+            values = self._extract_values_nousertype(subelement, manager)
+
+        return values
+    
+    def _extract_values_usertype(self, subelement, manager):
+        values = []
+        for subsubelement in subelement:
+            values.append(manager.from_xml(subsubelement))
+        return values
+    
+    def _extract_values_nousertype(self, subelement, manager):
+        return subelement.text.split(',')
+
+class Attribute(_XMLItem):
+
+    def _update_element(self, element, values, manager):
+        if not values:
+            return
+
+        text = []
+        for value in values:
+            if ',' in str(value):
+                raise ValueError, "Value %s contains ','. Cannot be serialised" % value
+            text.append(value)
+        text = ','.join(text)
+
+        element.set(self.xmlname, text)
+
+    def _extract_values(self, element, manager):
+        text = element.get(self.xmlname)
+        if text is None:
+            return []
+        return text.split(',')
+
+class _EmptyClass(object):
+    pass
+
+class XMLMapper(object):
+    
+    def __init__(self):
+        self._manager = Manager()
+        self._content = {}
+
+    def register_namespace(self, prefix, uri):
+        """
+        Registers a namespace to be used when loading and saving from/to XML.
+        
+        :arg prefix: prefix of the namespace
+        :arg uri: URI of the namespace
+        """
+        ElementTree.register_namespace(prefix, uri)
+
+    def register(self, klass, tag, *content):
+        self._manager.register(tag, klass)
+        
+        # Search for the content already registered classes
+        for base in klass.__bases__:
+            try:
+                basetag = self._manager.get_tag(base)
+            except ValueError:
+                continue
+            content += self._content[basetag]
+        
+        self._content[tag] = content
+
+    def is_registered(self, klass=None, tag=None):
+        return self._manager.is_registered(tag, klass)
+
+    def from_xml(self, element):
+        """
+        Loads an object from a XML element. A :exc:`ValueError` is raised if
+        no loader is found for the XML element.
+        
+        :arg element: XML element
+        """
+        klass = self._manager.get_class(element.tag)
+        content = self._content[element.tag]
+
+        recast = False
+        try:
+            obj = klass.__new__(klass)
+        except TypeError:
+            recast = True
+            obj = _EmptyClass()
+
+        for item in content:
+            item.load(obj, element, self)
+
+        if recast:
+            obj = klass(**obj.__dict__)
+
+        return obj
+
+    def to_xml(self, obj):
+        """
+        Saves an object as a XML element. A :exc:`ValueError` is raised if
+        no saver is found for the object.
+        
+        :arg obj: object
+        """
+        tag = self._manager.get_tag(obj.__class__)
+        content = self._content[tag]
+
+        element = ElementTree.Element(tag)
+
+        for item in content:
+            item.dump(obj, element, self)
+
+        return element
+
