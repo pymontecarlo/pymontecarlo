8f07457c9eaab4acddf868c351aabe80158965c2
hg <devnull@localhost>
2014-05-14 09:59:33 +0200

    Fix problem with positive exponent (with + sign).

--- pymontecarlo/ui/gui/util/widget.py
+++ pymontecarlo/ui/gui/util/widget.py
@@ -221,7 +221,7 @@ class NumericalLineEdit(QLineEdit):
 class MultiNumericalLineEdit(QLineEdit):
 
     _VALUE_SEPARATOR = ';'
-    _PATTERN = r'(?P<start>inf|[\de\.\-]*)(?:\:(?P<stop>[\de\.\-]*))?(?:\:(?P<step>[\de\.\-]*))?'
+    _PATTERN = r'(?P<start>inf|[\de\.+\-]*)(?:\:(?P<stop>[\de\.+\-]*))?(?:\:(?P<step>[\de\.+\-]*))?'
 
     @classmethod
     def _parse_text(cls, text):
