d6a07523ff83d140d3e08e5916c8e8f7348724a7
Philippe Pinard <philippe.pinard@gmail.com>
2017-01-31 22:19:00 +0000

    Validate samples. Simply material validation.

--- pymontecarlo/program/test_validator.py
+++ pymontecarlo/program/test_validator.py
@@ -9,10 +9,16 @@ import logging
 
 # Local modules.
 from pymontecarlo.program.validator import Validator
-from pymontecarlo.options.material import Material
+from pymontecarlo.options.material import Material, VACUUM
+from pymontecarlo.options.sample import \
+    Substrate, Inclusion, HorizontalLayers, VerticalLayers, Sphere
 from pymontecarlo.exceptions import ValidationError
 
 # Globals and constants variables.
+COPPER = Material.pure(29)
+ZINC = Material.pure(30)
+GALLIUM = Material.pure(31)
+GERMANIUM = Material.pure(32)
 
 class TestValidator(unittest.TestCase):
 
@@ -26,16 +32,86 @@ class TestValidator(unittest.TestCase):
 
     def testvalidate_material(self):
         material = Material('Pure Cu', {29: 1.0}, 8960.0)
-        material2 = self.v.validate_material(material)
-        self.assertEqual(material, material2)
+        self.v.validate_material(material)
 
     def testvalidate_material_exception(self):
         material = Material(' ', {120: 0.5}, -1.0)
         self.assertRaises(ValidationError, self.v.validate_material, material)
 
-        causes = set()
-        self.v._validate_material(material, causes)
-        self.assertEqual(4, len(causes))
+        errors = set()
+        self.v._validate_material(material, errors)
+        self.assertEqual(4, len(errors))
+
+    def testvalidate_sample_substrate(self):
+        sample = Substrate(COPPER)
+        self.v.validate_sample(sample)
+
+    def testvalidate_sample_substrate_exception(self):
+        sample = Substrate(VACUUM, float('inf'), float('nan'))
+        self.assertRaises(ValidationError, self.v.validate_sample, sample)
+
+        errors = set()
+        self.v._validate_sample(sample, errors)
+        self.assertEqual(3, len(errors))
+
+    def testvalidate_sample_inclusion(self):
+        sample = Inclusion(COPPER, ZINC, 1.0)
+        self.v.validate_sample(sample)
+
+    def testvalidate_sample_inclusion_exception(self):
+        sample = Inclusion(COPPER, ZINC, 0.0)
+        self.assertRaises(ValidationError, self.v.validate_sample, sample)
+
+        errors = set()
+        self.v._validate_sample(sample, errors)
+        self.assertEqual(1, len(errors))
+
+    def testvalidate_sample_horizontallayers(self):
+        sample = HorizontalLayers(COPPER)
+        sample.add_layer(ZINC, 1.0)
+        self.v.validate_sample(sample)
+
+    def testvalidate_sample_horizontallayers_empty_layer(self):
+        sample = HorizontalLayers(COPPER)
+        sample.add_layer(ZINC, 1.0)
+        sample.add_layer(VACUUM, 2.0)
+        self.v.validate_sample(sample)
+        self.assertEqual(1, len(sample.layers))
+
+    def testvalidate_sample_horizontallayers_exception(self):
+        sample = HorizontalLayers(COPPER)
+        sample.add_layer(ZINC, -1.0)
+        self.assertRaises(ValidationError, self.v.validate_sample, sample)
+
+        errors = set()
+        self.v._validate_sample(sample, errors)
+        self.assertEqual(1, len(errors))
+
+    def testvalidate_sample_verticallayers(self):
+        sample = VerticalLayers(COPPER, ZINC)
+        sample.add_layer(GALLIUM, 1.0)
+        self.v.validate_sample(sample)
+
+    def testvalidate_sample_verticallayers_exception(self):
+        sample = VerticalLayers(VACUUM, VACUUM)
+        sample.add_layer(GALLIUM, -1.0)
+        self.assertRaises(ValidationError, self.v.validate_sample, sample)
+
+        errors = set()
+        self.v._validate_sample(sample, errors)
+        self.assertEqual(3, len(errors))
+
+    def testvalidate_sample_sphere(self):
+        sample = Sphere(COPPER, 1.0)
+        self.v.validate_sample(sample)
+
+    def testvalidate_sample_sphere_exception(self):
+        sample = Sphere(VACUUM, -1.0)
+        self.assertRaises(ValidationError, self.v.validate_sample, sample)
+
+        errors = set()
+        self.v._validate_sample(sample, errors)
+        self.assertEqual(2, len(errors))
 
 if __name__ == '__main__': #pragma: no cover
     logging.getLogger().setLevel(logging.DEBUG)
--- pymontecarlo/program/validator.py
+++ pymontecarlo/program/validator.py
@@ -1,65 +1,148 @@
 """"""
 
 # Standard library modules.
+import math
 
 # Third party modules.
 import pyxray.descriptor
 
 # Local modules.
 from pymontecarlo.exceptions import ValidationError
+from pymontecarlo.options.material import VACUUM
+from pymontecarlo.options.sample import \
+    Substrate, Inclusion, HorizontalLayers, VerticalLayers, Sphere
 
 # Globals and constants variables.
 
 class Validator(object):
 
+    def __init__(self):
+        self.sample_validate_methods = {}
+        self.sample_validate_methods[Substrate] = self._validate_sample_substrate
+        self.sample_validate_methods[Inclusion] = self._validate_sample_inclusion
+        self.sample_validate_methods[HorizontalLayers] = self._validate_sample_horizontallayers
+        self.sample_validate_methods[VerticalLayers] = self._validate_sample_verticallayers
+        self.sample_validate_methods[Sphere] = self._validate_sample_sphere
+
     def validate_material(self, material):
         errors = set()
-        material = self._validate_material(material, errors)
+        self._validate_material(material, errors)
 
         if errors:
             raise ValidationError(*errors)
 
-        return material
-
     def _validate_material(self, material, errors):
-        material.name = \
-            self._validate_material_name(material.name, errors)
-        material.composition = \
-            self._validate_material_composition(material.composition, errors)
-        material.density_kg_per_m3 = \
-            self._validate_material_density_kg_per_m3(material.density_kg_per_m3, errors)
-        return material
+        if material is VACUUM:
+            return material
 
-    def _validate_material_name(self, name, errors):
-        name = name.strip()
-
-        if not name:
+        # Name
+        material.name = material.name.strip()
+        if not material.name:
             exc = ValueError('Name ({0:s}) must be at least one character'
-                             .format(name))
+                             .format(material.name))
             errors.add(exc)
 
-        return name
-
-    def _validate_material_composition(self, composition, errors):
-        for z in composition:
+        # Composition
+        for z in material.composition:
             try:
                 pyxray.descriptor.Element.validate(z)
             except ValueError as exc:
                 errors.add(exc)
 
-        total = sum(composition.values())
+        total = sum(material.composition.values())
         if total != 1.0:
             exc = ValueError('Total weight fraction ({0:g}) does not equal 1.0'
                              .format(total))
             errors.add(exc)
 
-        return composition
-
-    def _validate_material_density_kg_per_m3(self, density_kg_per_m3, errors):
-        if density_kg_per_m3 < 0:
+        # Density
+        if material.density_kg_per_m3 < 0:
             exc = ValueError('Density ({0:g}kg/m3) must be greater or equal to 0'
-                             .format(density_kg_per_m3))
+                             .format(material.density_kg_per_m3))
             errors.add(exc)
 
-        return density_kg_per_m3
+    def validate_sample(self, sample):
+        errors = set()
+        self._validate_sample(sample, errors)
 
+        if errors:
+            raise ValidationError(*errors)
+
+    def _validate_sample(self, sample, errors):
+        # Tilt
+        if not math.isfinite(sample.tilt_rad):
+            exc = ValueError('Tilt must be a finite number')
+            errors.add(exc)
+
+        # Rotation
+        if not math.isfinite(sample.rotation_rad):
+            exc = ValueError('Rotation must be a finite number')
+            errors.add(exc)
+
+        # Sample specific
+        sample_class = sample.__class__
+        if sample_class in self.sample_validate_methods:
+            method = self.sample_validate_methods[sample_class]
+            method(sample, errors)
+
+    def _validate_sample_substrate(self, sample, errors):
+        if sample.material is VACUUM:
+            exc = ValueError('Material cannot be VACUUM')
+            errors.add(exc)
+
+        self._validate_material(sample.material, errors)
+
+    def _validate_sample_inclusion(self, sample, errors):
+        self._validate_material(sample.substrate_material, errors)
+
+        self._validate_material(sample.inclusion_material, errors)
+
+        if sample.inclusion_diameter_m <= 0:
+            exc = ValueError('Diameter ({0:g} m) must be greater than 0'
+                             .format(sample.inclusion_diameter_m))
+            errors.add(exc)
+
+    def _validate_sample_layer(self, layer, errors):
+        self._validate_material(layer.material, errors)
+
+        if layer.thickness_m <= 0:
+            exc = ValueError('Thickness ({0:g} m) must be greater than 0'
+                             .format(layer.thickness_m))
+            errors.add(exc)
+
+    def _validate_sample_horizontallayers(self, sample, errors):
+        self._validate_material(sample.substrate_material, errors)
+
+        for layer in sample.layers:
+            self._validate_sample_layer(layer, errors)
+
+        if sample.layers and sample.layers[-1].material is VACUUM:
+            sample.layers.pop(-1)
+
+    def _validate_sample_verticallayers(self, sample, errors):
+        if sample.left_material is VACUUM:
+            exc = ValueError('Left material cannot be VACUUM')
+            errors.add(exc)
+
+        self._validate_material(sample.left_material, errors)
+
+        if sample.right_material is VACUUM:
+            exc = ValueError('Right material cannot be VACUUM')
+            errors.add(exc)
+
+        self._validate_material(sample.right_material, errors)
+
+        for layer in sample.layers:
+            self._validate_sample_layer(layer, errors)
+
+    def _validate_sample_sphere(self, sample, errors):
+        if sample.material is VACUUM:
+            exc = ValueError('Material cannot be VACUUM')
+            errors.add(exc)
+
+        self._validate_material(sample.material, errors)
+
+        if sample.diameter_m <= 0:
+            exc = ValueError('Diameter ({0:g} m) must be greater than 0'
+                             .format(sample.diameter_m))
+            errors.add(exc)
