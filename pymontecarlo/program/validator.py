de2c32330890d0b207164fc43eae2893ae31c476
Philippe Pinard <philippe.pinard@gmail.com>
2017-01-31 21:26:55 +0000

    Rename causes to errors

--- pymontecarlo/program/validator.py
+++ pymontecarlo/program/validator.py
@@ -13,53 +13,53 @@ from pymontecarlo.exceptions import ValidationError
 class Validator(object):
 
     def validate_material(self, material):
-        causes = set()
-        material = self._validate_material(material, causes)
+        errors = set()
+        material = self._validate_material(material, errors)
 
-        if causes:
-            raise ValidationError(*causes)
+        if errors:
+            raise ValidationError(*errors)
 
         return material
 
-    def _validate_material(self, material, causes):
+    def _validate_material(self, material, errors):
         material.name = \
-            self._validate_material_name(material.name, causes)
+            self._validate_material_name(material.name, errors)
         material.composition = \
-            self._validate_material_composition(material.composition, causes)
+            self._validate_material_composition(material.composition, errors)
         material.density_kg_per_m3 = \
-            self._validate_material_density_kg_per_m3(material.density_kg_per_m3, causes)
+            self._validate_material_density_kg_per_m3(material.density_kg_per_m3, errors)
         return material
 
-    def _validate_material_name(self, name, causes):
+    def _validate_material_name(self, name, errors):
         name = name.strip()
 
         if not name:
             exc = ValueError('Name ({0:s}) must be at least one character'
                              .format(name))
-            causes.add(exc)
+            errors.add(exc)
 
         return name
 
-    def _validate_material_composition(self, composition, causes):
+    def _validate_material_composition(self, composition, errors):
         for z in composition:
             try:
                 pyxray.descriptor.Element.validate(z)
             except ValueError as exc:
-                causes.add(exc)
+                errors.add(exc)
 
         total = sum(composition.values())
         if total != 1.0:
             exc = ValueError('Total weight fraction ({0:g}) does not equal 1.0'
                              .format(total))
-            causes.add(exc)
+            errors.add(exc)
 
         return composition
 
-    def _validate_material_density_kg_per_m3(self, density_kg_per_m3, causes):
+    def _validate_material_density_kg_per_m3(self, density_kg_per_m3, errors):
         if density_kg_per_m3 < 0:
             exc = ValueError('Density ({0:g}kg/m3) must be greater or equal to 0'
                              .format(density_kg_per_m3))
-            causes.add(exc)
+            errors.add(exc)
 
         return density_kg_per_m3
 
