8c034f5abd3127a1440669cfc2be534ad33e917c
Philippe Pinard <philippe.pinard@gmail.com>
2017-02-02 19:30:31 +0000

    Add missing builder

--- pymontecarlo/options/beam/__init__.py
+++ pymontecarlo/options/beam/__init__.py
@@ -3,4 +3,4 @@ Incident beams.
 """
 
 from .base import convert_diameter_fwhm_to_sigma, convert_diameter_sigma_to_fwhm
-from .gaussian import GaussianBeam
+from .gaussian import GaussianBeam, GaussianBeamBuilder
