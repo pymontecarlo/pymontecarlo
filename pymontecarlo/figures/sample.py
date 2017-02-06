9cb006454207fb4befb85f699ff9f9aadec7145e
Philippe Pinard <philippe.pinard@gmail.com>
2017-02-06 22:29:26 +0000

    Continue the implementation

--- pymontecarlo/figures/sample.py
+++ pymontecarlo/figures/sample.py
@@ -8,6 +8,8 @@ Figure to draw a sample.
 
 # Local modules.
 from pymontecarlo.figures.base import Figure
+from pymontecarlo.options.beam import GaussianBeam
+from pymontecarlo.options.sample import SubstrateSample
 
 # Globals and constants variables.
 
@@ -21,8 +23,36 @@ class SampleFigure(Figure):
         self.trajectories = []
 
         self.sample_draw_methods = {}
+        self.sample_draw_methods[SubstrateSample] = self._draw_sample_substrate
+
         self.beam_draw_methods = {}
+        self.beam_draw_methods[GaussianBeam] = self._draw_beam_gaussian
 
     def draw(self, ax):
+        if self.sample:
+            self._draw_sample(ax, self.sample)
 
-        raise NotImplementedError
+        for beam in self.beams:
+            self._draw_beam(ax, beam)
+
+    def _draw_sample(self, ax, sample):
+        sample_class = sample.__class__
+        if sample_class not in self.sample_draw_methods:
+            return
+
+        method = self.sample_draw_methods[sample_class]
+        method(ax, sample)
+
+    def _draw_sample_substrate(self, ax, sample):
+        pass
+
+    def _draw_beam(self, ax, beam):
+        beam_class = beam.__class__
+        if beam_class not in self.beam_draw_methods:
+            return
+
+        method = self.sample_draw_methods[beam_class]
+        method(ax, beam)
+
+    def _draw_beam_gaussian(self, ax, beam):
+        pass
