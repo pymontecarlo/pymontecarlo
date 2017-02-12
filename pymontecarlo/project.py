26c940893435fbb6a7be66e07676fb917ca9ff5f
Philippe Pinard <philippe.pinard@gmail.com>
2017-02-12 11:32:13 +0000

    Remove datarow method. Now handled by DataRow object.

--- pymontecarlo/project.py
+++ pymontecarlo/project.py
@@ -3,7 +3,6 @@ Project.
 """
 
 # Standard library modules.
-from collections import OrderedDict
 
 # Third party modules.
 
@@ -25,41 +24,6 @@ class Project(object):
         for simulation in self.simulations:
             for analysis in simulation.options.analyses:
                 analysis.calculate(simulation, tuple(self.simulations))
-
-    def iter_datarows(self, diff=False):
-        columns_options = OrderedDict()
-        columns_results = OrderedDict()
-        datarows_options = []
-        datarows_results = []
-        datarow_difference = set()
-
-        for simulation in self.simulations:
-            datarow = simulation.options.create_datarow()
-            columns_options.update(datarow)
-            datarows_options.append(datarow)
-            datarow_difference ^= set(datarow.items())
-
-            datarow_results = OrderedDict()
-            for result in simulation.results:
-                datarow = result.create_datarow()
-                columns_results.update(datarow)
-                datarow_results.update(datarow)
-            datarows_results.append(datarow_results)
-
-        datarow_difference = dict(datarow_difference)
-
-        for datarow_options, datarow_results in zip(datarows_options, datarows_results):
-            datarow = OrderedDict()
-
-            for column in columns_options:
-                if diff and column not in datarow_difference:
-                    continue
-                datarow[column] = datarow_options.get(column, None)
-
-            for column in columns_results:
-                datarow[column] = datarow_results.get(column, None)
-
-            yield datarow
 
     def save(self, filepath=None):
         if filepath is not None:
