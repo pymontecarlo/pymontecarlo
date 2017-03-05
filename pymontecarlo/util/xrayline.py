31df8491c4746b843b36479e2ca86cf83fc91c46
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-05 19:16:28 +0000

    Update to new pyxray version.

--- pymontecarlo/util/xrayline.py
+++ pymontecarlo/util/xrayline.py
@@ -27,9 +27,9 @@ class XrayLine(collections.namedtuple('XrayLine', ('element', 'transition'))):
     def __new__(cls, element, transition):
         element = pyxray.element(element)
         try:
-            transition = pyxray.transition(transition)
+            transition = pyxray.xray_transition(transition)
         except pyxray.NotFound:
-            transition = pyxray.transitionset(transition)
+            transition = pyxray.xray_transitionset(transition)
         return super().__new__(cls, element, transition)
 
     def __repr__(self):
@@ -38,10 +38,10 @@ class XrayLine(collections.namedtuple('XrayLine', ('element', 'transition'))):
     def __str__(self):
         symbol = pyxray.element_symbol(self.element)
 
-        if isinstance(self.transition, pyxray.Transition):
-            method = pyxray.transition_notation
+        if isinstance(self.transition, pyxray.XrayTransition):
+            method = pyxray.xray_transition_notation
         else:
-            method = pyxray.transitionset_notation
+            method = pyxray.xray_transitionset_notation
         notation = method(self.transition, _preferred_notation, _preferred_encoding)
 
         return '{0} {1}'.format(symbol, notation)
