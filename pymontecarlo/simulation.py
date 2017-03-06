ff159aeb0ea67dce04e570828234eb3013d0811a
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-06 23:31:53 +0000

    Add method to find result

--- pymontecarlo/simulation.py
+++ pymontecarlo/simulation.py
@@ -7,11 +7,18 @@ Simulation.
 # Third party modules.
 
 # Local modules.
+from pymontecarlo.util.cbook import find_by_type
 
 # Globals and constants variables.
 
 class Simulation(object):
 
-    def __init__(self, options):
+    def __init__(self, options, results=None):
         self.options = options
-        self.results = []
+
+        if results is None:
+            results = []
+        self.results = results.copy()
+
+    def find_result(self, result_class):
+        return find_by_type(self.results, result_class)
