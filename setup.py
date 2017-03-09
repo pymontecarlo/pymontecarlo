8963dc17a7ca03f484cfaffce293c7813691ccef
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-09 22:04:56 +0000

    Clean up setup and old modules

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-# Script information for the file.
-__author__ = "Philippe T. Pinard"
-__email__ = "philippe.pinard@gmail.com"
-__version__ = "0.1"
-__copyright__ = "Copyright (c) 2013 Philippe T. Pinard"
-__license__ = "GPL v3"
-
 # Standard library modules.
 import os
 
@@ -14,84 +7,31 @@ import os
 from setuptools import setup, find_packages
 
 # Local modules.
-from pymontecarlo.util.dist.command.clean import clean
-from pymontecarlo.util.dist.command.check import check
-
 import versioneer
+
 
 # Globals and constants variables.
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-packages = find_packages(exclude=('pymontecarlo.util.dist*',))
-namespace_packages = []
-with open(os.path.join(BASEDIR, 'requirements.txt')) as fp:
-    requirements = [line.strip() for line in fp]
+with open(os.path.join(BASEDIR, 'README.rst'), 'r') as fp:
+    LONG_DESCRIPTION = fp.read()
 
-entry_points = {'pymontecarlo.fileformat.options.material':
-                    ['Material = pymontecarlo.fileformat.options.material:MaterialXMLHandler'],
-                'pymontecarlo.fileformat.options.beam':
-                    ['PencilBeam = pymontecarlo.fileformat.options.beam:PencilBeamXMLHandler',
-                     'GaussianBeam = pymontecarlo.fileformat.options.beam:GaussianBeamXMLHandler',
-                     'GaussianExpTailBeam = pymontecarlo.fileformat.options.beam:GaussianExpTailBeamXMLHandler', ],
-                'pymontecarlo.fileformat.options.geometry':
-                    ['Substrate = pymontecarlo.fileformat.options.geometry:SubstrateXMLHandler',
-                     'Inclusion = pymontecarlo.fileformat.options.geometry:InclusionXMLHandler',
-                     'HorizontalLayers = pymontecarlo.fileformat.options.geometry:HorizontalLayersXMLHandler',
-                     'VerticalLayers = pymontecarlo.fileformat.options.geometry:VerticalLayersXMLHandler',
-                     'Sphere = pymontecarlo.fileformat.options.geometry:SphereXMLHandler'],
-                'pymontecarlo.fileformat.options.detector':
-                    ['BackscatteredElectronEnergyDetector = pymontecarlo.fileformat.options.detector:BackscatteredElectronEnergyDetectorXMLHandler',
-                     'TransmittedElectronEnergyDetector = pymontecarlo.fileformat.options.detector:TransmittedElectronEnergyDetectorXMLHandler',
-                     'BackscatteredElectronPolarAngularDetector = pymontecarlo.fileformat.options.detector:BackscatteredElectronPolarAngularDetectorXMLHandler',
-                     'TransmittedElectronPolarAngularDetector = pymontecarlo.fileformat.options.detector:TransmittedElectronPolarAngularDetectorXMLHandler',
-                     'BackscatteredElectronAzimuthalAngularDetector = pymontecarlo.fileformat.options.detector:BackscatteredElectronAzimuthalAngularDetectorXMLHandler',
-                     'TransmittedElectronAzimuthalAngularDetector = pymontecarlo.fileformat.options.detector:TransmittedElectronAzimuthalAngularDetectorXMLHandler',
-                     'BackscatteredElectronRadialDetector = pymontecarlo.fileformat.options.detector:BackscatteredElectronRadialDetectorXMLHandler',
-                     'PhotonSpectrumDetector = pymontecarlo.fileformat.options.detector:PhotonSpectrumDetectorXMLHandler',
-                     'PhotonDepthDetector = pymontecarlo.fileformat.options.detector:PhotonDepthDetectorXMLHandler',
-                     'PhiZDetector = pymontecarlo.fileformat.options.detector:PhiZDetectorXMLHandler',
-                     'PhotonRadialDetector = pymontecarlo.fileformat.options.detector:PhotonRadialDetectorXMLHandler',
-                     'PhotonEmissionMapDetector = pymontecarlo.fileformat.options.detector:PhotonEmissionMapDetectorXMLHandler',
-                     'PhotonIntensityDetector = pymontecarlo.fileformat.options.detector:PhotonIntensityDetectorXMLHandler',
-                     'TimeDetector = pymontecarlo.fileformat.options.detector:TimeDetectorXMLHandler',
-                     'ElectronFractionDetector = pymontecarlo.fileformat.options.detector:ElectronFractionDetectorXMLHandler',
-                     'ShowersStatisticsDetector = pymontecarlo.fileformat.options.detector:ShowersStatisticsDetectorXMLHandler',
-                     'TrajectoryDetector = pymontecarlo.fileformat.options.detector:TrajectoryDetectorXMLHandler', ],
-                'pymontecarlo.fileformat.options.limit':
-                    ['TimeLimit = pymontecarlo.fileformat.options.limit:TimeLimitXMLHandler',
-                     'ShowersLimit = pymontecarlo.fileformat.options.limit:ShowersLimitXMLHandler',
-                     'UncertaintyLimit = pymontecarlo.fileformat.options.limit:UncertaintyLimitXMLHandler'],
-                'pymontecarlo.fileformat.options.model':
-                    ['RegisteredModel = pymontecarlo.fileformat.options.model:RegisteredModelXMLHandler',
-                     'UserDefinedMassAbsorptionCoefficientModel = pymontecarlo.fileformat.options.model:UserDefinedMassAbsorptionCoefficientModelXMLHandler'],
+PACKAGES = find_packages()
 
-                'pymontecarlo.fileformat.results.result':
-                    ['PhotonIntensityResult = pymontecarlo.fileformat.results.result:PhotonIntensityResultHDF5Handler',
-                     'PhotonSpectrumResult = pymontecarlo.fileformat.results.result:PhotonSpectrumResultHDF5Handler',
-                     'PhotonDepthResult = pymontecarlo.fileformat.results.result:PhotonDepthResultHDF5Handler',
-                     'PhiZResult = pymontecarlo.fileformat.results.result:PhiZResultHDF5Handler',
-                     'PhotonRadialResult = pymontecarlo.fileformat.results.result:PhotonRadialResultHDF5Handler',
-                     'PhotonEmissionMapResult = pymontecarlo.fileformat.results.result:PhotonEmissionMapResultHDF5Handler',
-                     'TimeResult = pymontecarlo.fileformat.results.result:TimeResultHDF5Handler',
-                     'ShowersStatisticsResult = pymontecarlo.fileformat.results.result:ShowersStatisticsResultHDF5Handler',
-                     'ElectronFractionResult = pymontecarlo.fileformat.results.result:ElectronFractionResultHDF5Handler',
-                     'TrajectoryResult = pymontecarlo.fileformat.results.result:TrajectoryResultHDF5Handler',
-                     'BackscatteredElectronEnergyResult = pymontecarlo.fileformat.results.result:BackscatteredElectronEnergyResultHDF5Handler',
-                     'TransmittedElectronEnergyResult = pymontecarlo.fileformat.results.result:TransmittedElectronEnergyResultHDF5Handler',
-                     'BackscatteredElectronAzimuthalAngularResult = pymontecarlo.fileformat.results.result:BackscatteredElectronAzimuthalAngularResultHDF5Handler',
-                     'TransmittedElectronAzimuthalAngularResult = pymontecarlo.fileformat.results.result:TransmittedElectronAzimuthalAngularResultHDF5Handler',
-                     'BackscatteredElectronPolarAngularResult = pymontecarlo.fileformat.results.result:BackscatteredElectronPolarAngularResultHDF5Handler',
-                     'TransmittedElectronPolarAngularResult = pymontecarlo.fileformat.results.result:TransmittedElectronPolarAngularResultHDF5Handler',
-                     'BackscatteredElectronRadialResult = pymontecarlo.fileformat.results.result:BackscatteredElectronRadialResultHDF5Handler', ],
-                }
+SETUP_REQUIRES = ['setuptools']
+INSTALL_REQUIRES = ['pyparsing>=2.0.0', 'numpy', 'h5py', 'pyxray>=1.0.1',
+                    'more_itertools>=2.5.0', 'pint', 'uncertainties',
+                    'matplotlib']
+TESTS_REQUIRE = ['nose', 'coverage']
+EXTRAS_REQUIRE = {'doc': ['docutils', 'jinja2', 'sphinx', 'pybtex']}
 
 CMDCLASS = versioneer.get_cmdclass()
-CMDCLASS['clean'] = clean
-CMDCLASS["check"] = check
+
+ENTRY_POINTS = {'pymontecarlo.program': []}
 
 setup(name="pyMonteCarlo",
       version=versioneer.get_version(),
-      url='http://pymontecarlo.bitbucket.org',
+      url='https://github.com/pymontecarlo',
       description="Python interface for Monte Carlo simulation programs",
       author="Hendrix Demers and Philippe T. Pinard",
       author_email="hendrix.demers@mail.mcgill.ca and philippe.pinard@gmail.com",
@@ -105,15 +45,16 @@ setup(name="pyMonteCarlo",
                    'Topic :: Scientific/Engineering',
                    'Topic :: Scientific/Engineering :: Physics'],
 
-      packages=packages,
-      namespace_packages=namespace_packages,
+      packages=PACKAGES,
+
+      setup_requires=SETUP_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      tests_require=TESTS_REQUIRE,
+      extras_require=EXTRAS_REQUIRE,
 
       cmdclass=CMDCLASS,
 
-      setup_requires=['nose'],
-      install_requires=requirements,
-
-      entry_points=entry_points,
+      entry_points=ENTRY_POINTS,
 
       test_suite='nose.collector',
 )
