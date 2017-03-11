f06039aeec5bf89ad3d5c33aecf4aa8072e83fda
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-11 15:18:24 +0000

    Add executor for recalculate

--- pymontecarlo/project.py
+++ pymontecarlo/project.py
@@ -3,27 +3,50 @@ Project.
 """
 
 # Standard library modules.
+import threading
 
 # Third party modules.
 
 # Local modules.
+from pymontecarlo.util.future import FutureExecutor
 
 # Globals and constants variables.
+
+class RecalculateProjectExecutor(FutureExecutor):
+
+    def submit(self, project):
+        def target(token, project):
+            simulations = list(project.simulations)
+            count = len(simulations)
+
+            for i, simulation in enumerate(simulations):
+                if token.cancelled():
+                    break
+
+                progress = i / count
+                status = 'Calculating simulation {}'.format(simulation.identifier)
+                token.update(progress, status)
+
+                for analysis in simulation.options.analyses:
+                    analysis.calculate(simulation, tuple(simulations))
+
+        self._submit(target, project)
 
 class Project(object):
 
     def __init__(self, filepath=None):
         self.filepath = filepath
         self.simulations = []
+        self.lock = threading.Lock()
 
     def add_simulation(self, simulation):
         if simulation not in self.simulations:
             self.simulations.append(simulation)
 
     def recalculate(self):
-        for simulation in self.simulations:
-            for analysis in simulation.options.analyses:
-                analysis.calculate(simulation, tuple(self.simulations))
+        with RecalculateProjectExecutor() as executor:
+            future = executor.submit(self)
+            return future.result()
 
     def save(self, filepath=None):
         if filepath is not None:
