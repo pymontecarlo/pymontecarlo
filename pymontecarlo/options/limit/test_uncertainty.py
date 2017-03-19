4a3b04a33267c24d15200cf25df95d434bdfae77
Philippe Pinard <philippe.pinard@gmail.com>
2017-03-19 22:45:20 +0000

    Test with photon detector

--- pymontecarlo/options/limit/test_uncertainty.py
+++ pymontecarlo/options/limit/test_uncertainty.py
@@ -19,11 +19,12 @@ class TestUncertaintyLimit(TestCase):
     def setUp(self):
         super().setUp()
 
-        self.l = UncertaintyLimit(XrayLine(13, 'Ka1'), None, 0.02)
+        self.d = self.create_basic_photondetector()
+        self.l = UncertaintyLimit(XrayLine(13, 'Ka1'), self.d, 0.02)
 
     def testskeleton(self):
         self.assertEqual(XrayLine(13, 'Ka1'), self.l.xrayline)
-        self.assertIsNone(self.l.detector)
+        self.assertEqual(self.d, self.l.detector)
         self.assertAlmostEqual(0.02, self.l.uncertainty, 4)
 
     def test__repr__(self):
@@ -31,20 +32,20 @@ class TestUncertaintyLimit(TestCase):
         self.assertEqual(expected, repr(self.l))
 
     def test__eq__(self):
-        l = UncertaintyLimit(XrayLine(13, 'Ka1'), None, 0.02)
+        l = UncertaintyLimit(XrayLine(13, 'Ka1'), self.d, 0.02)
         self.assertEqual(l, self.l)
 
     def test__ne__(self):
-        l = UncertaintyLimit(XrayLine(14, 'Ka1'), None, 0.02)
+        l = UncertaintyLimit(XrayLine(14, 'Ka1'), self.d, 0.02)
         self.assertNotEqual(l, self.l)
 
-        l = UncertaintyLimit(XrayLine(13, 'La1'), None, 0.02)
+        l = UncertaintyLimit(XrayLine(13, 'La1'), self.d, 0.02)
         self.assertNotEqual(l, self.l)
 
         l = UncertaintyLimit(XrayLine(13, 'Ka1'), 'detector', 0.02)
         self.assertNotEqual(l, self.l)
 
-        l = UncertaintyLimit(XrayLine(13, 'Ka1'), None, 0.03)
+        l = UncertaintyLimit(XrayLine(13, 'Ka1'), self.d, 0.03)
         self.assertNotEqual(l, self.l)
 
 if __name__ == '__main__': #pragma: no cover
