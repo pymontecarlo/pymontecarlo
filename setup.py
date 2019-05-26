7c607abda1b6b9474d2c01ddeea71692a15ee647
Philippe Pinard <philippe.pinard@gmail.com>
2019-05-26 15:56:18 +0100

    Use dev for requirements

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(os.path.join(BASEDIR, 'requirements.txt'), 'r') as fp:
 
 EXTRAS_REQUIRE = {}
 with open(os.path.join(BASEDIR, 'requirements_dev.txt'), 'r') as fp:
-    EXTRAS_REQUIRE['develop'] = fp.read().splitlines()
+    EXTRAS_REQUIRE['dev'] = fp.read().splitlines()
 
 CMDCLASS = versioneer.get_cmdclass()
 
