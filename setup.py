c83e4635c0ffba9ceaeb31b32c0ab974c8276b1e
Philippe Pinard <philippe.pinard@gmail.com>
2021-12-29 11:53:19 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ import versioneer
 # Globals and constants variables.
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(BASEDIR, "README.rst"), "r") as fp:
+with open(os.path.join(BASEDIR, "README.md"), "r") as fp:
     LONG_DESCRIPTION = fp.read()
 
 PACKAGES = find_packages()
